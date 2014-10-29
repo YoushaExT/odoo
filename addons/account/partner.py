YoushaExT/odoo

commit 48465aa94bcc3124579c65f5e63f52bfaac7d031

[FIX] account: fiscal position may map single tax to multiple taxes
(This is a regression in Odoo 8)

It should now work when a fiscal position replaces 1 tax with
several taxes (for example : Fiscal position "Intra-EU B2B" in l10n_fr)

Fixes #2261, manual merge of PR #3316

addons/account/partner.py
@@ -83,7 +83,6 @@ class account_fiscal_position(osv.osv):
                 if t.tax_src_id == tax:
                     if t.tax_dest_id:
                         result |= t.tax_dest_id
-                    break
             else:
                 result |= tax
         return result

commit 04833809bfb45cff11961ba8fcd37c08c67f0fda

[FIX] account: tax mapping logic was broken by 48465aa
The original tax should not be applied
if at least one destination tax was
applied. It should only be applied if
explicitly included in target taxes.

(Re-)Fixes #2261

addons/account/partner.py
@@ -79,11 +79,13 @@ class account_fiscal_position(osv.osv):
     def map_tax(self, taxes):
         result = self.env['account.tax'].browse()
         for tax in taxes:
+            tax_count = 0
             for t in self.tax_ids:
                 if t.tax_src_id == tax:
                     if t.tax_dest_id:
+                        tax_count += 1
                         result |= t.tax_dest_id
-            else:
+            if not tax_count:
                 result |= tax
         return result
 