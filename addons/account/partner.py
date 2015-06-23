YoushaExT/odoo

commit 55d84b8518da71f1fed43b10b94ffc96a98ff8d8

[FIX] account: change delivery address
The default fiscal position on sale order, is set according to[by priority]:
[1] the delivery
[2] the partner

opw:641883

addons/account/partner.py
@@ -113,15 +113,15 @@ class account_fiscal_position(osv.osv):
         part_obj = self.pool['res.partner']
         partner = part_obj.browse(cr, uid, partner_id, context=context)
 
-        # partner manually set fiscal position always win
-        if partner.property_account_position:
-            return partner.property_account_position.id
-
         # if no delivery use invocing
         if delivery_id:
             delivery = part_obj.browse(cr, uid, delivery_id, context=context)
         else:
             delivery = partner
+
+        # partner manually set fiscal position always win
+        if delivery.property_account_position or partner.property_account_position:
+            return delivery.property_account_position or partner.property_account_position
 
         domains = [[('auto_apply', '=', True), ('vat_required', '=', partner.vat_subjected)]]
         if partner.vat_subjected:

commit 4c291381d8ff88939d9598c8948e06a695425234

[FIX] account: get_fiscal_position is expected to return an id
Not a browse record.
This is a regression introduced by the revision 55d84b8518da71f1fed43b10b94ffc96a98ff8d8

opw-643188
fixes #7231

addons/account/partner.py
@@ -121,7 +121,7 @@ class account_fiscal_position(osv.osv):
 
         # partner manually set fiscal position always win
         if delivery.property_account_position or partner.property_account_position:
-            return delivery.property_account_position or partner.property_account_position
+            return delivery.property_account_position.id or partner.property_account_position.id
 
         domains = [[('auto_apply', '=', True), ('vat_required', '=', partner.vat_subjected)]]
         if partner.vat_subjected: