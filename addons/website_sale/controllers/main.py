YoushaExT/odoo

commit 9ca79fb92d1fe1d50d9cdf2cd53a9347723cd1c2

[FIX] website_sale: transaction error
When an error occurs during the transaction, the customer must receive
a message to inform him.

opw:645705

addons/website_sale/controllers/main.py
@@ -766,8 +766,8 @@ class website_sale(http.Controller):
                 message = '<p>%s</p>' % _('Your transaction is waiting confirmation.')
                 if tx.acquirer_id.post_msg:
                     message += tx.acquirer_id.post_msg
-            else:
-                message = '<p>%s</p>' % _('Your transaction is waiting confirmation.')
+            elif state == 'error':
+                message = '<p>%s</p>' % _('An error occured during the transaction.')
             validation = tx.acquirer_id.validation
 
         return {

commit 887082a41109004c3876bb18338e71644a3f584e

[FIX] website_sale: error in message
Error introduced by 9ca79fb92d1fe1d50d9cdf2cd53a9347723cd1c2

addons/website_sale/controllers/main.py
@@ -767,7 +767,7 @@ class website_sale(http.Controller):
                 if tx.acquirer_id.post_msg:
                     message += tx.acquirer_id.post_msg
             elif state == 'error':
-                message = '<p>%s</p>' % _('An error occured during the transaction.')
+                message = '<p>%s</p>' % _('An error occurred during the transaction.')
             validation = tx.acquirer_id.validation
 
         return {