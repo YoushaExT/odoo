YoushaExT/odoo

commit 8409baaa4f916963822e06b495ebe70cb7c10b38

[FIX] stock_account: merge the Reference/Description
When invoices are created from pickings, and that the user chooses to group by
partner, we make sure to keep the Reference/Description of all the source
documents. This is exactly what is done for the Source Document field.

opw-646903

addons/stock_account/stock.py
@@ -311,9 +311,15 @@ class stock_picking(osv.osv):
                 invoices[key] = invoice_id
             else:
                 invoice = invoice_obj.browse(cr, uid, invoices[key], context=context)
+                merge_vals = {}
                 if not invoice.origin or invoice_vals['origin'] not in invoice.origin.split(', '):
                     invoice_origin = filter(None, [invoice.origin, invoice_vals['origin']])
-                    invoice.write({'origin': ', '.join(invoice_origin)})
+                    merge_vals['origin'] = ', '.join(invoice_origin)
+                if not invoice.name or invoice_vals['name'] not in invoice.name.split(', '):
+                    invoice_name = filter(None, [invoice.name, invoice_vals['name']])
+                    merge_vals['name'] = ', '.join(invoice_name)
+                if merge_vals:
+                    invoice.write(merge_vals)
 
             invoice_line_vals = move_obj._get_invoice_line_vals(cr, uid, move, partner, inv_type, context=context)
             invoice_line_vals['invoice_id'] = invoices[key]

commit e44d37009e4ccfadbffdd5ae8d2e8a57c6d4c3a5

[FIX] stock_account: remove traceback introduced by 8409baa
invoice_vals['name'] may not be defined yet.

addons/stock_account/stock.py
@@ -240,7 +240,7 @@ class stock_picking(osv.osv):
             @return: object of the partner to invoice
         """
         return picking.partner_id and picking.partner_id.id
-        
+
     def action_invoice_create(self, cr, uid, ids, journal_id, group=False, type='out_invoice', context=None):
         """ Creates invoice based on the invoice state selected for picking.
         @param journal_id: Id of journal
@@ -315,7 +315,7 @@ class stock_picking(osv.osv):
                 if not invoice.origin or invoice_vals['origin'] not in invoice.origin.split(', '):
                     invoice_origin = filter(None, [invoice.origin, invoice_vals['origin']])
                     merge_vals['origin'] = ', '.join(invoice_origin)
-                if not invoice.name or invoice_vals['name'] not in invoice.name.split(', '):
+                if invoice_vals.get('name', False) and (not invoice.name or invoice_vals['name'] not in invoice.name.split(', ')):
                     invoice_name = filter(None, [invoice.name, invoice_vals['name']])
                     merge_vals['name'] = ', '.join(invoice_name)
                 if merge_vals: