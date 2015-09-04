YoushaExT/odoo

commit f2addd8d916c2aa4bbfc497716be0add43c9c0d8

[FIX] models: copy_data call in new API.
Context are frozendict in the new API.
Therefore, they cannot be altered.
Any attempt to alter a frozendict
will lead to a crash.

Fixes #7362

openerp/models.py
@@ -4718,8 +4718,10 @@ class BaseModel(object):
             context = {}
 
         # avoid recursion through already copied records in case of circular relationship
-        seen_map = context.setdefault('__copy_data_seen', {})
-        if id in seen_map.setdefault(self._name, []):
+        if '__copy_data_seen' not in context:
+            context = dict(context, __copy_data_seen=defaultdict(list))
+        seen_map = context['__copy_data_seen']
+        if id in seen_map[self._name]:
             return
         seen_map[self._name].append(id)
 
@@ -4789,8 +4791,10 @@ class BaseModel(object):
             context = {}
 
         # avoid recursion through already copied records in case of circular relationship
-        seen_map = context.setdefault('__copy_translations_seen',{})
-        if old_id in seen_map.setdefault(self._name,[]):
+        if '__copy_data_seen' not in context:
+            context = dict(context, __copy_data_seen=defaultdict(list))
+        seen_map = context['__copy_data_seen']
+        if old_id in seen_map[self._name]:
             return
         seen_map[self._name].append(old_id)
 

commit 16089c8bebabd8669739a4c223387e0d67f1c29e

[FIX] models: oversight introduced in f2addd8d916c2aa4bbfc497716be0add43c9c0d8

openerp/models.py
@@ -4791,9 +4791,9 @@ class BaseModel(object):
             context = {}
 
         # avoid recursion through already copied records in case of circular relationship
-        if '__copy_data_seen' not in context:
-            context = dict(context, __copy_data_seen=defaultdict(list))
-        seen_map = context['__copy_data_seen']
+        if '__copy_translations_seen' not in context:
+            context = dict(context, __copy_translations_seen=defaultdict(list))
+        seen_map = context['__copy_translations_seen']
         if old_id in seen_map[self._name]:
             return
         seen_map[self._name].append(old_id)