YoushaExT/odoo

commit d62be7761e794d45ced9fcadcd67ec25cf0eeabc

[IMP] web_settings_dashboard: speed up Inviation dashboard
Searching on `log_ids` field is slow when there is a lot of users.
Count the number of active/pending users directly in sql.
Only show the last 10 pending users. Add a link to the full list view.

addons/web_settings_dashboard/controllers/main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import http
+from openerp.exceptions import AccessError
 from openerp.http import request
 
 
@@ -8,21 +9,39 @@ class WebSettingsDashboard(http.Controller):
 
     @http.route('/web_settings_dashboard/data', type='json', auth='user')
     def web_settings_dashboard_data(self, **kw):
+        if not request.env.user.has_group('base.group_erp_manager'):
+            raise AccessError("Access Denied")
 
         installed_apps = request.env['ir.module.module'].search_count([
-            ('application', '=',  True),
+            ('application', '=', True),
             ('state', 'in', ['installed', 'to upgrade', 'to remove'])
         ])
-        active_users = request.env['res.users'].search_count([('active', '=', True), ('log_ids', '!=', False)])
-        pending_users = request.env['res.users'].search([('log_ids', '=', False)], order="create_date desc")
+        cr = request.cr
+        cr.execute("""
+            SELECT exists(SELECT 1 FROM res_users_log WHERE create_uid=u.id), count(1)
+              FROM res_users u
+             WHERE active=true
+          GROUP BY 1
+        """)
+        counts = dict(cr.fetchall())
 
+        cr.execute("""
+           SELECT id, login
+             FROM res_users u
+            WHERE active=true
+              AND NOT exists(SELECT 1 FROM res_users_log WHERE create_uid=u.id)
+         ORDER BY id desc
+            LIMIT 10
+        """)
+        pending_users = cr.fetchall()
         return {
             'apps': {
                 'installed_apps': installed_apps
             },
             'users_info': {
-                'active_users': active_users,
-                'pending_users': zip(pending_users.mapped('id'), pending_users.mapped('login')),
+                'active_users': counts[True],
+                'pending_count': counts[False],
+                'pending_users': pending_users,
                 'user_form_view_id': request.env['ir.model.data'].xmlid_to_res_id("base.view_users_form"),
             },
         }

addons/web_settings_dashboard/static/src/js/dashboard.js
@@ -72,6 +72,7 @@ var DashboardInvitations = Widget.extend({
         'click .o_web_settings_dashboard_invitations': 'send_invitations',
         'click .o_web_settings_dashboard_access_rights': 'on_access_rights_clicked',
         'click .o_web_settings_dashboard_user': 'on_user_clicked',
+        'click .o_web_settings_dashboard_more': 'on_more',
     },
     init: function(parent, data){
         this.data = data;
@@ -128,6 +129,20 @@ var DashboardInvitations = Widget.extend({
             views: [[this.data.user_form_view_id, 'form']],
             res_id: user_id,
         }
+        this.do_action(action,{
+            on_reverse_breadcrumb: function(){ return self.reload();}
+        });
+    },
+    on_more: function(e) {
+        e.preventDefault();
+        var action = {
+            type: 'ir.actions.act_window',
+            view_type: 'form',
+            view_mode: 'tree,form',
+            res_model: 'res.users',
+            domain: [['log_ids', '=', false]],
+            views: [[false, 'list'], [false, 'form']],
+        };
         this.do_action(action,{
             on_reverse_breadcrumb: function(){ return self.reload();}
         });

addons/web_settings_dashboard/static/src/xml/dashboard.xml
@@ -66,6 +66,10 @@
                         <t t-foreach="pending_users" t-as="pending">
                             <a href="#"><div class="o_web_settings_dashboard_user" t-att-data-user-id="pending[0]"> <t t-esc="pending[1]"/></div></a>
                         </t>
+                        <t t-if="pending_users.length &lt; widget.data.pending_count">
+                            <br/>
+                            <a href="#"><div class="o_web_settings_dashboard_more"><t t-esc="widget.data.pending_count - pending_users.length"/> more</div></a>
+                        </t>
                     </t>
                 </small>
             </div>

commit 6a3f8c44f2d44e67d55e5a457ab2db7b5314366d

[FIX] web_settings_dashboard
Error introduced by d62be77.

Fixes #9439

addons/web_settings_dashboard/controllers/main.py
@@ -39,8 +39,8 @@ class WebSettingsDashboard(http.Controller):
                 'installed_apps': installed_apps
             },
             'users_info': {
-                'active_users': counts[True],
-                'pending_count': counts[False],
+                'active_users': counts.get(True, 0),
+                'pending_count': counts.get(False, 0),
                 'pending_users': pending_users,
                 'user_form_view_id': request.env['ir.model.data'].xmlid_to_res_id("base.view_users_form"),
             },