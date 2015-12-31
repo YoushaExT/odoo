YoushaExT/odoo

commit 9a02459d3714f8efbbd0c5ffdd89a7d395e52f80

[FIX] web: keep o2m current page in correct range when going through different form view records
For example, when being on page 4 of a o2m then switching to next form
view record, if this new record did not have 4 pages for this o2m, the
o2m was shown empty (and if there was no page, the o2m became unusable)

addons/web/static/src/js/view_form.js
@@ -4606,7 +4606,14 @@ instance.web.form.One2ManyListView = instance.web.ListView.extend({
         this.dataset.evict_record(record.get('id'));
 
         return this._super(record);
-    }
+    },
+    reload_content: function () {
+        this.page = Math.min(
+            this.page,
+            Math.ceil(this.dataset.size() / this.limit()) - 1
+        );
+        return this._super.apply(this, arguments);
+    },
 });
 instance.web.form.One2ManyGroups = instance.web.ListView.Groups.extend({
     setup_resequence_rows: function () {

commit d4ce088f8c5f7204dce908dc6067e3548f503430

[FIX] web: o2m pager when empty
Commit 9a02459d3714f8efbbd0c5ffdd89a7d395e52f80 introduced a bug which led to be on page -1 on
empty o2m. So when adding a record to such a o2m, the reload content
function chose to stay on page -1 therefore not displaying the new
added record.

addons/web/static/src/js/view_form.js
@@ -4607,10 +4607,10 @@ instance.web.form.One2ManyListView = instance.web.ListView.extend({
         return this._super(record);
     },
     reload_content: function () {
-        this.page = Math.min(
+        this.page = Math.max(0, Math.min(
             this.page,
             Math.ceil(this.dataset.size() / this.limit()) - 1
-        );
+        ));
         return this._super.apply(this, arguments);
     },
 });