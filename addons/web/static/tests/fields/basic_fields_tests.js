YoushaExT/odoo

commit de3bf5579702d99a477383fd881a6a2386a41276

[FIX] web: calendar timezone with user configuration
The client must receive the tzOffset to apply this on all hours.
To use the date picker we must change the date to apply the change
in the user's tzOffset, without this change the result is wrong.
For datetime widget we must change like it to avoid max stack error.

The server send the tzOffset, if it's undefined, by default the client
use the browser time offset.

Every test are change to use tzOffset.

A test is added in calendar to use the click by position for the
fullcalendar lib. This test create and drag and drop an event to check
timezone error and error when we use default value in the context.

Use formating date like 2026-04-04T08:00:00Z instead of 2026-04-04 08:00:00
is important for phantomjs, because it's crash without information if the
formating is not the standard format.

For the parsing, when the client receive a server date the date is utc
formating but when it's parsed from the client is in the user's timezone

addons/web/static/src/js/fields/basic_fields.js
@@ -267,8 +267,9 @@ var FieldDate = InputField.extend({
         if (this.mode === 'edit') {
             this.datewidget = this._makeDatePicker();
             this.datewidget.on('datetime_changed', this, function () {
-                if (!this.datewidget.get_value().isSame(this.value)) {
-                    this._setValue(this.datewidget.get_value());
+                var value = this._getDateWidgetValue();
+                if (!value.isSame(this.value)) {
+                    this._setValue(value);
                 }
             });
             def = this.datewidget.appendTo('<div>').done(function () {
@@ -284,6 +285,15 @@ var FieldDate = InputField.extend({
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
+
+    /**
+     * return the datepicker value
+     *
+     * @private
+     */
+    _getDateWidgetValue: function () {
+        return this.datewidget.get_value();
+    },
 
     /**
      * Instantiates a new DateWidget datepicker.
@@ -315,13 +325,46 @@ var FieldDateTime = FieldDate.extend({
     //--------------------------------------------------------------------------
 
     /**
+     * return the datepicker value
+     *
+     * @private
+     */
+    _getDateWidgetValue: function () {
+        return this.datewidget.get_value().add(-this.getSession().tzOffset, 'minutes');
+    },
+
+    /**
      * Instantiates a new DateTimeWidget datepicker rather than DateWidget.
      *
      * @override
      * @private
      */
     _makeDatePicker: function () {
-        return new datepicker.DateTimeWidget(this, {defaultDate: this.value});
+        var value = this.value && this.value.clone().add(this.getSession().tzOffset, 'minutes');
+        return new datepicker.DateTimeWidget(this, {defaultDate: value});
+    },
+
+    /**
+     * Set the datepicker to the right value rather than the default one.
+     *
+     * @override
+     * @private
+     */
+    _renderEdit: function () {
+        var value = this.value && this.value.clone().add(this.getSession().tzOffset, 'minutes');
+        this.datewidget.set_value(value);
+        this.$input = this.datewidget.$input;
+    },
+
+    /**
+     * FieldDateTime overrides _formatValue to use the session timezone
+     *
+     * @override
+     * @private
+     * @param {Moment} value
+     */
+    _formatValue: function (value) {
+        return field_utils.format.datetime(value, {timezone: true});
     },
 });
 

addons/web/static/src/js/widgets/date_picker.js
@@ -60,7 +60,7 @@ var DateWidget = Widget.extend({
         }
     },
     get_value: function() {
-        return this.get('value');
+        return this.get('value').clone();
     },
     set_value_from_ui: function() {
         var value = this.$input.val() || false;
@@ -84,10 +84,10 @@ var DateWidget = Widget.extend({
         }
     },
     parse_client: function(v) {
-        return field_utils.parse[this.type_of_date](v, {type: this.type_of_date});
+        return field_utils.parse[this.type_of_date](v, {timezone: false});
     },
     format_client: function(v) {
-        return field_utils.format[this.type_of_date](v, {type: this.type_of_date});
+        return field_utils.format[this.type_of_date](v, {timezone: false});
     },
     set_datetime_default: function() {
         //when opening datetimepicker the date and time by default should be the one from

addons/web/static/tests/fields/basic_fields_tests.js
@@ -1000,7 +1000,10 @@ QUnit.module('basic_fields', {
             },
             translateParameters: {  // Avoid issues due to localization formats
               date_format: '%m/%d/%Y',
-            }
+            },
+            session: {
+                tzOffset: 120
+            },
         });
 
         assert.strictEqual(form.$('.o_form_field_date').text(), '02/03/2017',
@@ -1094,10 +1097,13 @@ QUnit.module('basic_fields', {
             translateParameters: {  // Avoid issues due to localization formats
                 date_format: '%m/%d/%Y',
                 time_format: '%H:%M:%S',
-            }
+            },
+            session: {
+                tzOffset: 120
+            },
         });
 
-        var expectedDateString = "02/08/2017 10:00:00";
+        var expectedDateString = "02/08/2017 12:00:00"; // 10:00:00 without timezone
         assert.strictEqual(form.$('.o_form_field_date').text(), expectedDateString,
             'the datetime should be correctly displayed in readonly');
 
@@ -1148,10 +1154,13 @@ QUnit.module('basic_fields', {
             translateParameters: {  // Avoid issues due to localization formats
                 date_format: '%m/%d/%Y',
                 time_format: '%H:%M:%S',
-            }
+            },
+            session: {
+                tzOffset: 120
+            },
         });
 
-        var expectedDateString = "02/08/2017 10:00:00";
+        var expectedDateString = "02/08/2017 12:00:00"; // 10:00:00 without timezone
         var $cell = list.$('tr.o_data_row td:not(.o_list_record_selector)').first();
         assert.strictEqual($cell.text(), expectedDateString,
             'the datetime should be correctly displayed in readonly');
@@ -1597,7 +1606,7 @@ QUnit.module('basic_fields', {
         assert.ok($cell.hasClass('o_edit_mode'), 'should be set as edit mode');
         assert.strictEqual($cell.find('input').val(), 'yop',
             'should have the corect value in internal input');
-        $cell.find('input').val('new').trigger('input');;
+        $cell.find('input').val('new').trigger('input');
 
         // save
         list.$buttons.find('.o_list_button_save').click();

addons/account/static/tests/reconciliation_tests.js
@@ -384,12 +384,12 @@ var auto_reconciliation = {
 var data_for_manual_reconciliation_widget = {
     '[null,null]': {
         'customers': [
-            {'account_id': 287, 'partner_name': "Agrolait", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-14 12:30:31.256629", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 8, 'account_name': "101200 Account Receivable"},
-            {'account_id': 7, 'partner_name': "Camptocamp", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-13 14:24:55.246469", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 12, 'account_name': "101200 Account Receivable"}
+            {'account_id': 287, 'partner_name': "Agrolait", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-14 12:30:31", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 8, 'account_name': "101200 Account Receivable"},
+            {'account_id': 7, 'partner_name': "Camptocamp", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-13 14:24:55", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 12, 'account_name': "101200 Account Receivable"}
         ],
         'accounts': [
             {
-                'account_id': 283, 'account_name': "101000 Current Assets", 'currency_id': 3, 'max_date': "2017-02-16 14:32:04.833986", 'last_time_entries_checked': "2017-02-16", 'account_code': "101000",
+                'account_id': 283, 'account_name': "101000 Current Assets", 'currency_id': 3, 'max_date': "2017-02-16 14:32:04", 'last_time_entries_checked': "2017-02-16", 'account_code': "101000",
                 'reconciliation_proposition': [
                     {'account_id': 283, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-02-16", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 8, 'account_name': "101000 Current Assets", 'name': "BNK1/2017/0006: Customer Payment", 'partner_name': "Agrolait", 'total_amount_currency_str': "", 'id': 399, 'credit': 1000.0, 'journal_id': [3, "Bank"], 'amount_str': "$ 1,000.00", 'debit': 0.0, 'account_code': "101000", 'ref': "", 'already_paid': false},
                     {'account_id': 283, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-03-18", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 8, 'account_name': "101000 Current Assets", 'name': "INV/2017/0006", 'partner_name': "Agrolait", 'total_amount_currency_str': "", 'id': 402, 'credit': 0.0, 'journal_id': [1, "Customer Invoices"], 'amount_str': "$ 1,000.00", 'debit': 1000.0, 'account_code': "101000", 'ref': "", 'already_paid': false}
@@ -403,20 +403,20 @@ var data_for_manual_reconciliation_widget = {
                     {'account_id': 284, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-02-16", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 8, 'account_name': "101000 Current Assets", 'name': "BNK1/999: Customer Payment", 'partner_name': "Agrolait", 'total_amount_currency_str': "", 'id': 999, 'credit': 1000.0, 'journal_id': [3, "Bank"], 'amount_str': "$ 1,000.00", 'debit': 0.0, 'account_code': "111100", 'ref': "", 'already_paid': false},
                     {'account_id': 284, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-03-18", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 8, 'account_name': "101000 Current Assets", 'name': "INV/998", 'partner_name': "Agrolait", 'total_amount_currency_str': "", 'id': 998, 'credit': 0.0, 'journal_id': [1, "Customer Invoices"], 'amount_str': "$ 1,000.00", 'debit': 1000.0, 'account_code': "111100", 'ref': "", 'already_paid': false}
                 ],
-                'currency_id': 3, 'max_date': "2017-02-14 12:36:05.014302", 'last_time_entries_checked': null, 'account_code': "111100", 'partner_id': 8, 'account_name': "Account Payable"
+                'currency_id': 3, 'max_date': "2017-02-14 12:36:05", 'last_time_entries_checked': null, 'account_code': "111100", 'partner_id': 8, 'account_name': "Account Payable"
             }, {
                 'account_id': 284, 'partner_name': "Camptocamp",
                 'reconciliation_proposition': [
                     {'account_id': 284, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-02-16", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 12, 'account_name': "101000 Current Assets", 'name': "BNK1/1999: Customer Payment", 'partner_name': "Camptocamp", 'total_amount_currency_str': "", 'id': 1999, 'credit': 1000.0, 'journal_id': [3, "Bank"], 'amount_str': "$ 1,000.00", 'debit': 0.0, 'account_code': "111100", 'ref': "", 'already_paid': false},
                     {'account_id': 284, 'account_type': "other", 'amount_currency_str': "", 'currency_id': false, 'date_maturity': "2017-03-18", 'date': "2017-02-16", 'total_amount_str': "$ 1,000.00", 'partner_id': 12, 'account_name': "101000 Current Assets", 'name': "INV/1998", 'partner_name': "Camptocamp", 'total_amount_currency_str': "", 'id': 1998, 'credit': 0.0, 'journal_id': [1, "Customer Invoices"], 'amount_str': "$ 1,000.00", 'debit': 1000.0, 'account_code': "111100", 'ref': "", 'already_paid': false}
                 ],
-                'currency_id': 3, 'max_date': "2017-02-14 12:36:05.014302", 'last_time_entries_checked': null, 'account_code': "111100", 'partner_id': 12, 'account_name': "Account Payable"
+                'currency_id': 3, 'max_date': "2017-02-14 12:36:05", 'last_time_entries_checked': null, 'account_code': "111100", 'partner_id': 12, 'account_name': "Account Payable"
             }
         ]
     },
     '["partner",null,"receivable"]': [
-        {'account_id': 287, 'partner_name': "Agrolait", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-14 12:30:31.256629", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 8, 'account_name': "101200 Account Receivable"},
-        {'account_id': 287, 'partner_name': "Camptocamp", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-13 14:24:55.246469", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 12, 'account_name': "101200 Account Receivable"}
+        {'account_id': 287, 'partner_name': "Agrolait", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-14 12:30:31", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 8, 'account_name': "101200 Account Receivable"},
+        {'account_id': 287, 'partner_name': "Camptocamp", 'reconciliation_proposition': [], 'currency_id': 3, 'max_date': "2017-02-13 14:24:55", 'last_time_entries_checked': null, 'account_code': "101200", 'partner_id': 12, 'account_name': "101200 Account Receivable"}
     ]
 };
 

addons/web/models/ir_http.py
@@ -5,6 +5,8 @@ import json
 
 from odoo import models
 from odoo.http import request
+import pytz
+import datetime
 
 import odoo
 
@@ -24,6 +26,7 @@ class Http(models.AbstractModel):
         version_info = odoo.service.common.exp_version()
         return {
             "session_id": request.session.sid,
+            "tzOffset": datetime.datetime.now(pytz.timezone(self._context.get('tz', 'UTC'))).utcoffset().total_seconds() / 60,
             "uid": request.session.uid,
             "is_system": request.env.user._is_system(),
             "is_superuser": request.env.user._is_superuser(),

addons/web/static/src/js/core/session.js
@@ -285,6 +285,10 @@ var Session = core.Class.extend(mixins.EventDispatcherMixin, {
         var result = _.extend({}, window.odoo.session_info);
         delete result.session_id;
         _.extend(this, result);
+        if (!this.tzOffset) {
+            // send by server to have the user timezone and not the browser timezone
+            this.tzOffset = -new Date().getTimezoneOffset();
+        }
         return $.when();
     },
     check_session_id: function () {

addons/web/static/src/js/fields/field_utils.js
@@ -79,9 +79,12 @@ function formatDate(value) {
  * @params {Moment|false}
  * @returns {string}
  */
-function formatDateTime(value) {
+function formatDateTime(value, options) {
     if (!value) {
         return "";
+    }
+    if (!options || !('timezone' in options) || options.timezone) {
+        value = value.clone().add(session.tzOffset, 'minutes');
     }
     var l10n = core._t.database.parameters;
     var date_format = time.strftime_to_moment_format(l10n.date_format);
@@ -267,24 +270,26 @@ function formatSelection(value, field) {
  * @params {string}
  * @returns {Moment|false} Moment date object
  */
-function parseDate(value) {
+function parseDate(value, field, options) {
     if (!value) {
         return false;
     }
     var date_pattern = time.strftime_to_moment_format(core._t.database.parameters.date_format);
     var date_pattern_wo_zero = date_pattern.replace('MM','M').replace('DD','D');
-    var date = moment.utc(value, [date_pattern, date_pattern_wo_zero, moment.ISO_8601], true);
-    if (date.isValid() && date.year() >= 1900) {
-        date.toJSON = function () {return this.utc().format('YYYY-MM-DD');};
-        return date;
+    var date;
+    if (options && options.isUTC) {
+        date = moment.utc(value);
+    } else {
+        date = moment.utc(value, [date_pattern, date_pattern_wo_zero, moment.ISO_8601], true);
     }
-    date = moment.utc(value, [date_pattern, date_pattern_wo_zero, moment.ISO_8601]);
-    if (date.isValid()) {
+    if (date.isValid() && date.year() >= 1900) {
         if (date.year() === 0) {
             date.year(moment.utc().year());
         }
         if (date.year() >= 1900) {
-            date.toJSON = function () {return this.utc().format('YYYY-MM-DD');};
+            date.toJSON = function () {
+                return this.format('YYYY-MM-DD');
+            };
             return date;
         }
     }
@@ -298,7 +303,7 @@ function parseDate(value) {
  * @params {string}
  * @returns {Moment|false} Moment date object
  */
-function parseDateTime(value) {
+function parseDateTime(value, field, options) {
     if (!value) {
         return false;
     }
@@ -308,20 +313,24 @@ function parseDateTime(value) {
         time_pattern_wo_zero = time_pattern.replace('HH','H').replace('mm','m').replace('ss','s');
     var pattern1 = date_pattern + ' ' + time_pattern;
     var pattern2 = date_pattern_wo_zero + ' ' + time_pattern_wo_zero;
-    var datetime = moment.utc(value, [pattern1, pattern2, moment.ISO_8601], true);
-    if (datetime.isValid() && datetime.year() >= 1900) {
-        datetime.toJSON = function () {
-            return this.utc().format('YYYY-MM-DD HH:mm:ss');
-        };
-        return datetime;
+    var datetime;
+    if (options && options.isUTC) {
+        // phatomjs crash if we don't use this format 
+        datetime = moment.utc(value.replace(' ', 'T') + 'Z');
+    } else {
+        datetime = moment.utc(value, [pattern1, pattern2, moment.ISO_8601], true);
+        if (options && options.timezone) {
+            datetime.add(-session.tzOffset, 'minutes');
+        }
     }
-    datetime = moment.utc(value, [pattern1, pattern2, moment.ISO_8601]);
     if (datetime.isValid()) {
         if (datetime.year() === 0) {
             datetime.year(moment.utc().year());
         }
         if (datetime.year() >= 1900) {
-            datetime.toJSON = function () {return this.utc().format('YYYY-MM-DD HH:mm:ss');};
+            datetime.toJSON = function () {
+                return this.format('YYYY-MM-DD HH:mm:ss');
+            };
             return datetime;
         }
     }

addons/web/static/src/js/views/basic/basic_model.js
@@ -1023,10 +1023,10 @@ var BasicModel = AbstractModel.extend({
                         });
                     } else if (field.type === 'date') {
                         // process data: convert into a moment instance
-                        record._changes[name] = fieldUtils.parse.date(val);
+                        record._changes[name] = fieldUtils.parse.date(val, field, {isUTC: true});
                     } else if (field.type === 'datetime') {
                         // process datetime: convert into a moment instance
-                        record._changes[name] = fieldUtils.parse.datetime(val);
+                        record._changes[name] = fieldUtils.parse.datetime(val, field, {isUTC: true});
                     } else {
                         record._changes[name] = val;
                     }
@@ -2195,10 +2195,10 @@ var BasicModel = AbstractModel.extend({
                         });
                     } else if (field.type === 'date') {
                         // process date: convert into a moment instance
-                        record._changes[name] = fieldUtils.parse.date(result[name]);
+                        record._changes[name] = fieldUtils.parse.date(result[name], field, {isUTC: true});
                     } else if (field.type === 'datetime') {
                         // process datetime: convert into a moment instance
-                        record._changes[name] = fieldUtils.parse.datetime(result[name]);
+                        record._changes[name] = fieldUtils.parse.datetime(result[name], field, {isUTC: true});
                     } else {
                         record._changes[name] = result[name];
                     }
@@ -2268,10 +2268,10 @@ var BasicModel = AbstractModel.extend({
                 }
             } else if (field.type === 'date') {
                 // process data: convert into a moment instance
-                record[fieldName] = fieldUtils.parse.date(val);
+                record[fieldName] = fieldUtils.parse.date(val, field, {isUTC: true});
             } else if (field.type === 'datetime') {
                 // process datetime: convert into a moment instance
-                record[fieldName] = fieldUtils.parse.datetime(val);
+                record[fieldName] = fieldUtils.parse.datetime(val, field, {isUTC: true});
             }
         });
     },

commit fa9443067389e2685d78e72a528d915f65334331

[FIX] web: date_picker empty input field
When you try to empty a date or datetime field, you get a traceback
saying that 'clone' and 'isSame' are not functions.

This error occurs when you empty the field because the value is 'false'
and you cannot call 'clone' or 'isSame' on false.

To fix this, we ensure that value is not false before calling these
methods on it.

This bug has been introduced in rev: https://github.com/odoo/odoo/commit/de3bf5579702d99a477383fd881a6a2386a41276

addons/web/static/src/js/fields/basic_fields.js
@@ -268,7 +268,7 @@ var FieldDate = InputField.extend({
             this.datewidget = this._makeDatePicker();
             this.datewidget.on('datetime_changed', this, function () {
                 var value = this._getDateWidgetValue();
-                if (!value.isSame(this.value)) {
+                if ((!value && this.value) || (value && !value.isSame(this.value))) {
                     this._setValue(value);
                 }
             });
@@ -330,7 +330,8 @@ var FieldDateTime = FieldDate.extend({
      * @private
      */
     _getDateWidgetValue: function () {
-        return this.datewidget.get_value().add(-this.getSession().tzOffset, 'minutes');
+        var value = this.datewidget.get_value();
+        return value && value.add(-this.getSession().tzOffset, 'minutes');
     },
 
     /**

addons/web/static/src/js/widgets/date_picker.js
@@ -60,7 +60,7 @@ var DateWidget = Widget.extend({
         }
     },
     get_value: function() {
-        return this.get('value').clone();
+        return this.get('value') && this.get('value').clone();
     },
     set_value_from_ui: function() {
         var value = this.$input.val() || false;

addons/web/static/tests/fields/basic_fields_tests.js
@@ -1082,6 +1082,41 @@ QUnit.module('basic_fields', {
         list.destroy();
     });
 
+    QUnit.test('date field remove value', function (assert) {
+        assert.expect(4);
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch:'<form string="Partners"><field name="date"/></form>',
+            res_id: 1,
+            mockRPC: function (route, args) {
+                if (route === '/web/dataset/call_kw/partner/write') {
+                    assert.strictEqual(args.args[1].date, false, 'the correct value should be saved');
+                }
+                return this._super.apply(this, arguments);
+            },
+            translateParameters: {  // Avoid issues due to localization formats
+                date_format: '%m/%d/%Y',
+            },
+        });
+
+        // switch to edit mode
+        form.$buttons.find('.o_form_button_edit').click();
+        assert.strictEqual(form.$('.o_datepicker_input').val(), '02/03/2017',
+            'the date should be correct in edit mode');
+
+        $('.o_datepicker_input').val('').trigger('input').trigger('change');
+        assert.ok(!form.$('.o_datepicker_input').val());
+
+        // save
+        form.$buttons.find('.o_form_button_save').click();
+        assert.strictEqual(form.$('.o_form_field_date').text(), '',
+            'the selected date should be displayed after saving');
+        form.destroy();
+    });
+
 
     QUnit.module('FieldDatetime');
 
@@ -1204,6 +1239,45 @@ QUnit.module('basic_fields', {
             'the selected datetime should be displayed after saving');
 
         list.destroy();
+    });
+
+    QUnit.test('datetime field remove value', function (assert) {
+        assert.expect(4);
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch:'<form string="Partners"><field name="datetime"/></form>',
+            res_id: 1,
+            mockRPC: function (route, args) {
+                if (route === '/web/dataset/call_kw/partner/write') {
+                    assert.strictEqual(args.args[1].datetime, false, 'the correct value should be saved');
+                }
+                return this._super.apply(this, arguments);
+            },
+            translateParameters: {  // Avoid issues due to localization formats
+                date_format: '%m/%d/%Y',
+                time_format: '%H:%M:%S',
+            },
+            session: {
+                tzOffset: 120
+            },
+        });
+
+        // switch to edit mode
+        form.$buttons.find('.o_form_button_edit').click();
+        assert.strictEqual(form.$('.o_datepicker_input').val(), '02/08/2017 12:00:00',
+            'the date time should be correct in edit mode');
+
+        $('.o_datepicker_input').val('').trigger('input').trigger('change');
+        assert.ok(!form.$('.o_datepicker_input').val());
+
+        // save
+        form.$buttons.find('.o_form_button_save').click();
+        assert.strictEqual(form.$('.o_form_field_date').text(), '',
+            'the selected date should be displayed after saving');
+        form.destroy();
     });
 
 