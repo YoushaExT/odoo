YoushaExT/odoo

commit 80e2e488c43ce79ef96a68323478afa89bcdd1fd

[FIX] web: return a deferred in m2o quick create
The rev. https://github.com/odoo/odoo/commit/f231db51b1995918781119da7bddfba8a9d1daaa
accidentally modified the `quickCreate` return value type, which was previously a deferred.

This function is overriden in Studio to catch the deferred so the previous type has
been restored.

addons/web/static/src/js/fields/relational_fields.js
@@ -219,10 +219,16 @@ var FieldMany2One = AbstractField.extend({
     /**
      * @private
      * @param {string} name
+     * @returns {Deferred} resolved after the name_create or when the slowcreate
+     *                     modal is closed.
      */
     _quickCreate: function (name) {
         var self = this;
-        var slowCreate = this._searchCreatePopup.bind(this, "form", false, this._createContext(name));
+        var def = $.Deferred();
+        var slowCreate = function () {
+            var dialog = self._searchCreatePopup.bind(self, "form", false, self._createContext(name));
+            dialog.on('closed', self, def.resolve.bind(def));
+        };
         if (this.nodeOptions.quick_create) {
             this.trigger_up('mutexify', {
                 action: function () {
@@ -235,12 +241,16 @@ var FieldMany2One = AbstractField.extend({
                         if (self.mode === "edit") {
                             self.reinitialize({id: result[0], display_name: result[1]});
                         }
-                    }, slowCreate);
+                        def.resolve();
+                    }).fail(function () {
+                        slowCreate();
+                    });
                 },
             });
         } else {
             slowCreate();
         }
+        return def;
     },
     /**
      * @private

commit f6db5782af64d47395cb286861f525fe5e690780

[FIX] web: make "slow" create of m2o fields work
There were some coding mistakes since some changes introduced by
https://github.com/odoo/odoo/commit/80e2e488c43ce79ef96a68323478afa89bcdd1fd

addons/web/static/src/js/fields/relational_fields.js
@@ -225,7 +225,7 @@ var FieldMany2One = AbstractField.extend({
         var self = this;
         var def = $.Deferred();
         var slowCreate = function () {
-            var dialog = self._searchCreatePopup.bind(self, "form", false, self._createContext(name));
+            var dialog = self._searchCreatePopup("form", false, self._createContext(name));
             dialog.on('closed', self, def.resolve.bind(def));
         };
         if (this.nodeOptions.quick_create) {
@@ -393,7 +393,7 @@ var FieldMany2One = AbstractField.extend({
      */
     _searchCreatePopup: function (view, ids, context) {
         var self = this;
-        new dialogs.SelectCreateDialog(this, _.extend({}, this.nodeOptions, {
+        return new dialogs.SelectCreateDialog(this, _.extend({}, this.nodeOptions, {
             res_model: this.field.relation,
             domain: this.record.getDomain({fieldName: this.name}),
             context: _.extend({}, this.record.getContext(this.recordParams), context || {}),

addons/web/static/src/js/views/view_dialogs.js
@@ -403,6 +403,7 @@ var SelectCreateDialog = ViewDialog.extend({
             },
         })).open();
         dialog.on('closed', this, this.close.bind(this));
+        return dialog;
     },
 });
 

addons/web/static/tests/fields/relational_fields_tests.js
@@ -1289,6 +1289,38 @@ QUnit.module('relational_fields', {
         form.destroy();
     });
 
+    QUnit.test('slow create on a many2one', function (assert) {
+        assert.expect(1);
+
+        var form = createView({
+            View: FormView,
+            model: 'partner',
+            data: this.data,
+            arch:
+                '<form>' +
+                    '<sheet>' +
+                        '<field name="product_id" options="{\'quick_create\': False}"/>' +
+                    '</sheet>' +
+                '</form>',
+            archs: {
+                'product,false,form':
+                    '<form>' +
+                        '<field name="name"/>' +
+                    '</form>',
+            },
+        });
+
+        form.$('.o_field_many2one input').focus();
+        form.$('.o_field_many2one input').val('new partner').trigger('keyup').trigger('focusout');
+
+        $('.modal .modal-footer .btn-primary').first().click();
+
+        assert.strictEqual($('.modal:visible:last .o_form_view').length, 1,
+            'a new modal should be opened and contain a form view');
+
+        form.destroy();
+    });
+
     QUnit.test('no_create option on a many2one', function (assert) {
         assert.expect(1);
 