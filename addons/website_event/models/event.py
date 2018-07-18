YoushaExT/odoo

commit a82661c1be95c926b04ff08604ddf94c72feb4b3

[FIX] website_event: put location in calendar event only if there is an address
Otherwse there is a traceback. Anyway no need to put a void location.

addons/website_event/models/event.py
@@ -167,9 +167,10 @@ class Event(models.Model):
             'action': 'TEMPLATE',
             'text': self.name,
             'dates': url_date_start + '/' + url_date_stop,
-            'location': self.sudo().address_id.contact_address.replace('\n', ' '),
             'details': self.name,
         })
+        if self.address_id:
+            params['location'] = self.sudo().address_id.contact_address.replace('\n', ' ')
         google_url = GOOGLE_CALENDAR_URL + params
         iCal_url = '/event/%s/ics?%s' % (slug(self), params)
         return {'google_url': google_url, 'iCal_url': iCal_url}

commit 6d91c77cf320c7e5474962f97ce7a3a19a67d463

[FIX] website_event: prevent crash when address_id is set
This fixes a bug introduced with commit a82661c1be95c926b04ff08604ddf94c72feb4b3 where it tried to set a parameter after it had been url encoded.

addons/website_event/models/event.py
@@ -163,14 +163,15 @@ class Event(models.Model):
     def _get_event_resource_urls(self, attendees):
         url_date_start = datetime.strptime(self.date_begin, DEFAULT_SERVER_DATETIME_FORMAT).strftime('%Y%m%dT%H%M%SZ')
         url_date_stop = datetime.strptime(self.date_end, DEFAULT_SERVER_DATETIME_FORMAT).strftime('%Y%m%dT%H%M%SZ')
-        params = werkzeug.url_encode({
+        params = {
             'action': 'TEMPLATE',
             'text': self.name,
             'dates': url_date_start + '/' + url_date_stop,
             'details': self.name,
-        })
+        }
         if self.address_id:
-            params['location'] = self.sudo().address_id.contact_address.replace('\n', ' ')
-        google_url = GOOGLE_CALENDAR_URL + params
-        iCal_url = '/event/%s/ics?%s' % (slug(self), params)
+            params.update(location=self.sudo().address_id.contact_address.replace('\n', ' '))
+        encoded_params = werkzeug.url_encode(params)
+        google_url = GOOGLE_CALENDAR_URL + encoded_params
+        iCal_url = '/event/%s/ics?%s' % (slug(self), encoded_params)
         return {'google_url': google_url, 'iCal_url': iCal_url}