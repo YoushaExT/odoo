YoushaExT/odoo

commit 418b04f627ade46ee169f098c4311e7c9a90a968

[FIX] website: improve/fix menu, copy on unlink, clone page, unique_path, sale_report, ...
Fix bug from the initial poc of JOV

Fix menu creation
   - creating a menu would create a 'container' menu in the DB (.create is call without website_id)
   then writing on it would copy (if condition to cow) the menu with a website_id leaving the first
   one as a menu container unused.
   - website_menu should always have a website_id
     we dont want to support the multiwebsite system that allow to have generic/specific menu
     unlink/write is useless now since we always have a website_id

Make unique_path website dependent, 2 distinct website can have a page with same name

Make Sale report multiwebsite compliant

Make website_id on sale_order / account.invoice as a related stored from partner_id.

Co-authored-by: Derie Romain <rde@odoo.com>
Co-authored-by: Kersten Jérémy <jke@odoo.com>

addons/website_event/models/event.py
@@ -84,7 +84,7 @@ class Event(models.Model):
                     event.menu_id.unlink()
                 elif event.website_menu:
                     if not event.menu_id:
-                        root_menu = self.env['website.menu'].create({'name': event.name})
+                        root_menu = self.env['website.menu'].create({'name': event.name, 'website_id': event.id})
                         event.menu_id = root_menu
                     for sequence, (name, url, xml_id) in enumerate(event._get_menu_entries()):
                         event._create_menu(sequence, name, url, xml_id)
@@ -99,6 +99,7 @@ class Event(models.Model):
             'url': url,
             'parent_id': self.menu_id.id,
             'sequence': sequence,
+            'website_id': self.id,
         })
         return menu
 

addons/auth_signup/models/res_config_settings.py
@@ -11,8 +11,8 @@ class ResConfigSettings(models.TransientModel):
 
     auth_signup_reset_password = fields.Boolean(string='Enable password reset from Login page', config_parameter='auth_signup.reset_password')
     auth_signup_uninvited = fields.Selection([
-        ('b2b', 'On invitation (B2B)'),
-        ('b2c', 'Free sign up (B2C)'),
+        ('b2b', 'On invitation'),
+        ('b2c', 'Free sign up'),
     ], string='Customer Account', default='b2b', config_parameter='auth_signup.invitation_scope')
     auth_signup_template_user_id = fields.Many2one('res.users', string='Template user for new users created through signup',
                                                    config_parameter='base.template_portal_user_id')

addons/sale/models/res_config_settings.py
@@ -52,8 +52,8 @@ class ResConfigSettings(models.TransientModel):
     module_website_sale_digital = fields.Boolean("Sell digital products - provide downloadable content on your customer portal")
 
     auth_signup_uninvited = fields.Selection([
-        ('b2b', 'On invitation (B2B)'),
-        ('b2c', 'Free sign up (B2C)'),
+        ('b2b', 'On invitation'),
+        ('b2c', 'Free sign up'),
     ], string='Customer Account', default='b2b', config_parameter='auth_signup.invitation_scope')
 
     module_delivery = fields.Boolean("Shipping Costs")

addons/website/data/website_data.xml
@@ -10,7 +10,8 @@
         </record>
 
         <record id="main_menu" model="website.menu">
-            <field name="name">Top Menu</field>
+          <field name="name">Top Menu</field>
+          <field name="website_id" ref="default_website"/>
         </record>
 
         <record id="menu_homepage" model="website.menu">
@@ -18,6 +19,7 @@
             <field name="url">/</field>
             <field name="parent_id" ref="website.main_menu"/>
             <field name="sequence" type="int">10</field>
+            <field name="website_id" ref="default_website"/>
         </record>
 
         <record id="menu_contactus" model="website.menu">
@@ -25,6 +27,7 @@
             <field name="url">/contactus</field>
             <field name="parent_id" ref="website.main_menu"/>
             <field name="sequence" type="int">60</field>
+            <field name="website_id" ref="default_website"/>
         </record>
 
         <!-- split between ir_ui_view & website_page record to keep external_id on ir_ui_view -->

addons/website/data/website_demo.xml
@@ -176,7 +176,7 @@ response = request.render("website.template_partner_comment", {
         <record id="website2_menu_contactus" model="website.menu">
             <field name="name">Contact us</field>
             <field name="url">/contactus</field>
-            <field name="parent_id" ref="website.main_menu"/>
+            <field name="parent_id" search="[('website_id', '=', ref('website2')), ('parent_id', '=', False)]"/>
             <field name="sequence" type="int">60</field>
             <field name="website_id" ref="website2"/>
             <field name="page_id" ref="website2_contactus_page" />

addons/website/models/ir_attachment.py
@@ -14,6 +14,13 @@ class Attachment(models.Model):
     website_url = fields.Char(string="Website URL", related='local_url', deprecated=True)
 
     @api.model
+    def create(self, vals):
+        website = self.env['website'].get_current_website()
+        if website and 'website_id' not in vals and 'not_force_website_id' not in self.env.context:
+            vals['website_id'] = website.id
+        return super(Attachment, self).create(vals)
+
+    @api.model
     def get_serving_groups(self):
         return super(Attachment, self).get_serving_groups() + ['website.group_website_designer']
 

addons/website/models/ir_http.py
@@ -179,7 +179,7 @@ class Http(models.AbstractModel):
                     return response
             except Exception as e:
                 if 'werkzeug' in config['dev_mode'] and (not isinstance(exception, QWebException) or not exception.qweb.get('cause')):
-                    raise
+                    raise e
                 exception = e
 
             values = dict(

addons/website/models/ir_ui_view.py
@@ -75,9 +75,8 @@ class View(models.Model):
                     # reuse the COW mechanism to create
                     # website-specific copies, it will take
                     # care of creating pages and menus.
-                    view.with_context(website_id=website.id).write({'key': '%s [website %s]' % (view.key, website.id)})
+                    view.with_context(website_id=website.id).write({'name': view.name})
 
-        self |= self.with_context(active_test=False).search([('key', 'in', self.filtered('key').mapped('key'))])
         result = super(View, self).unlink()
         self.clear_caches()
         return result
@@ -108,9 +107,6 @@ class View(models.Model):
             new_page = page.copy({
                 'view_id': new_view.id,
             })
-            for menu in page.menu_ids:
-                # trigger COW
-                menu.write({'page_id': new_page.id})
 
     @api.model
     def get_related_views(self, key, bundles=False):

commit 21ed025aa6564c2c1b1a1fc9a48c8ff245c07597

[FIX] website_event: Impossible to track proposal on website
Programming error introduced by 418b04f627ade46ee169f098c4311e7c9a90a968

Steps to reproduce the bug:

- Create an event with category='conference'
- Checked Track Proposals on Website
- Save it

An error was raised saying that the website with the id of the event didn't exist.

Further, with the fix but without a website_id specified, checked Track proposal didn't
add the menu to track proposals on the conference.

opw:1904078

closes odoo/odoo#28688

addons/website_event/models/event.py
@@ -81,7 +81,7 @@ class Event(models.Model):
                     event.menu_id.unlink()
                 elif event.website_menu:
                     if not event.menu_id:
-                        root_menu = self.env['website.menu'].create({'name': event.name, 'website_id': event.id})
+                        root_menu = self.env['website.menu'].create({'name': event.name, 'website_id': event.website_id.id})
                         event.menu_id = root_menu
                     for sequence, (name, url, xml_id) in enumerate(event._get_menu_entries()):
                         event._create_menu(sequence, name, url, xml_id)
@@ -96,7 +96,7 @@ class Event(models.Model):
             'url': url,
             'parent_id': self.menu_id.id,
             'sequence': sequence,
-            'website_id': self.id,
+            'website_id': self.website_id.id,
         })
         return menu
 

addons/website_event/views/event_views.xml
@@ -31,7 +31,7 @@
         <field name="inherit_id" ref="event.view_event_form"/>
         <field name="arch" type="xml">
             <field name="organizer_id" position="before">
-                <field name="website_id" options="{'no_create': True}" groups="website.group_multi_website"/>
+                <field name="website_id" options="{'no_create': True}"  attrs="{'required': [('website_menu','=',True)]}" groups="website.group_multi_website"/>
             </field>
             <button name="toggle_active" position="before">
                 <field name="website_url" invisible="1"/>