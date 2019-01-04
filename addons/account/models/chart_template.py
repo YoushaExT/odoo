YoushaExT/odoo

commit 0b70ea78ca5b704382f11cb6560271ece1bc93e1

[FIX] account: install a chart of account on another company
1. Create odoo instance with "--without-demo=ALL" option.
2. Install Accounting module.
3. Add Chart of Accounts manually (e.g. code 111111 Bank and Cash and 222222 Other Income)
4. Create Journal.
5. Create Journal Entries.
6. Activate multi-company.
7. Set new company to current user and set the new company as current company.
8. Install accounting localization: Accounting >> Configuration >> Settings >> Install More Packages >> (install any localization module)

Before this commit, there was an error because when installing any module,
the environment in which it happens is in uid = 1 and doesn't have a context anymore
Thus, when arriving in the charts of accounts code, the env's user is still 1
Which doesn't mean anything in V12, since it is the system user

After this commit, the user is retrieved from the request

OPW 1911677

closes odoo/odoo#29310

addons/account/models/chart_template.py
@@ -5,6 +5,7 @@ from odoo import api, fields, models, _
 from odoo import SUPERUSER_ID
 from odoo.exceptions import UserError
 from odoo.tools import pycompat
+from odoo.http import request
 
 import logging
 
@@ -164,7 +165,13 @@ class AccountChartTemplate(models.Model):
         of accounts had been created for it yet.
         """
         self.ensure_one()
-        company = self.env.user.company_id
+        if request:
+            company_id = request.env.user.company_id.id
+            company = self.env['res.company'].browse(company_id)
+        else:
+            # fallback to company of current user, most likely __system__
+            # (won't work well for multi-company)
+            company = self.env.user.company_id
         # If we don't have any chart of account on this company, install this chart of account
         if not company.chart_template_id:
             self.load_for_current_company(15.0, 15.0)
@@ -178,7 +185,13 @@ class AccountChartTemplate(models.Model):
         rights.
         """
         self.ensure_one()
-        company = self.env.user.company_id
+        if request:
+            company_id = request.env.user.company_id.id
+            company = self.env['res.company'].browse(company_id)
+        else:
+            # fallback to company of current user, most likely __system__
+            # (won't work well for multi-company)
+            company = self.env.user.company_id
         # Ensure everything is translated to the company's language, not the user's one.
         self = self.with_context(lang=company.partner_id.lang)
         if not self.env.user._is_admin():

commit d472551344c78138ba4394ff873c8a1a1fee5a6f

[FIX] account: deadlock on install
- When installing an accounting l10n module, the method `try_loading_for_current_company`
  is called and could cause a deadlock.
  The deadlock is caused by the fact that if the installation is started
  via a web request the code tries to retrieve the current user's
  company by doing `request.env.user.company_id`.

  This causes issues since the installation code has its own postgresql
  cursor while calling `request.env.user.company_id` will create its own
  cursor.

  So two cursors exists for the same Odoo worker.
  The installation cursor asks for a lock on `res_users` while the
  second cursor (the request one) will try to read on `res_users` and
  wait the first lock to be dropped.

  The lock is never dropped since the python code isn't running anymore
  waiting for the `request.env.user.company_id` to be completed.
  Causing the deadlock.

  To fix the issue, we avoid the creation/usage of a cursor created by
  `requesŧ`.
  So to retrieve the current user's company, we read the user id on the
  request's session, then browse the `res.user` record with the
  installation cusror.

  Fix issue introduced by commit 0b70ea78ca5b704382f11cb6560271ece1bc93e1

closes odoo/odoo#29927

addons/account/models/chart_template.py
@@ -165,9 +165,10 @@ class AccountChartTemplate(models.Model):
         of accounts had been created for it yet.
         """
         self.ensure_one()
-        if request and request.db:
-            company_id = request.env.user.company_id.id
-            company = self.env['res.company'].browse(company_id)
+        # do not use `request.env` here, it can cause deadlocks
+        if request and request.session.uid:
+            current_user = self.env['res.users'].browse(request.uid)
+            company = current_user.company_id
         else:
             # fallback to company of current user, most likely __system__
             # (won't work well for multi-company)
@@ -185,9 +186,10 @@ class AccountChartTemplate(models.Model):
         rights.
         """
         self.ensure_one()
-        if request and request.db:
-            company_id = request.env.user.company_id.id
-            company = self.env['res.company'].browse(company_id)
+        # do not use `request.env` here, it can cause deadlocks
+        if request and request.session.uid:
+            current_user = self.env['res.users'].browse(request.uid)
+            company = current_user.company_id
         else:
             # fallback to company of current user, most likely __system__
             # (won't work well for multi-company)