YoushaExT/odoo

commit 7c63f39db086f68fdce5b7d97bdcded74c125178

[FIX] event_sale: on change of events product in SO
Have a product that is an event, with the price of 1500.
Create a ticket for this product in an event, with the price of 600.
In a SO, add a line with this ticket.

Before this commit, when you change the product, to another product that
is also an event, the event, the ticket and the price stayed the ones of
the first product.

Now, the event, the ticket and the price are updated when the product is
modified.

opw-1940368

addons/event_sale/models/sale_order.py
@@ -69,3 +69,12 @@ class SaleOrderLine(models.Model):
     def product_uom_change(self):
         if not self.event_ticket_id:
             super(SaleOrderLine, self).product_uom_change()
+
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        super(SaleOrderLine, self).product_id_change()
+        self.event_id = None
+        self.event_ticket_id = None
+        if self.product_id.event_ok:
+            self.price_unit = 0.0

commit 323d2deecb8e8114d991037d640f04181bb3f95f

[FIX] event_sale: result returned by super not propagated
Issue introduced in commit : 7c63f39db086f68fdce5b7d97bdcded74c125178

opw-1947735

closes odoo/odoo#31600

Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

addons/event_sale/models/sale_order.py
@@ -73,8 +73,9 @@ class SaleOrderLine(models.Model):
     @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
-        super(SaleOrderLine, self).product_id_change()
+        result = super(SaleOrderLine, self).product_id_change()
         self.event_id = None
         self.event_ticket_id = None
         if self.product_id.event_ok:
             self.price_unit = 0.0
+        return result