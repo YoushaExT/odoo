YoushaExT/odoo

commit f5cf28356f77165e5a032c82d48fefba00a9ece2

[FIX] account: do not allow a user to have both B2B and B2C groups
We base ourselves on commit f79b4389aca, which added a check on user groups.
The tax-included and tax excluded displays are meant to be exclusive,
but wily users can add themselves to both groups using the res_users view
in debug mode.

opw 1950833

closes odoo/odoo#32200

Signed-off-by: Jérémy Kersten (jke) <jke@openerp.com>

addons/account/i18n/account.pot
@@ -655,6 +655,12 @@ msgid "A tax fiscal position could be defined only one time on same taxes."
 msgstr ""
 
 #. module: account
+#: code:addons/account/models/res_users.py:17
+#, python-format
+msgid "A user cannot have both Tax B2B and Tax B2C"
+msgstr ""
+
+#. module: account
 #: model:res.groups,name:account.group_warning_account
 msgid "A warning can be set on a partner (Account)"
 msgstr ""

addons/account/models/res_users.py
@@ -1,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class Users(models.Model):
+    _inherit = "res.users"
+
+    @api.multi
+    @api.constrains('groups_id')
+    def _check_one_user_type(self):
+        super(Users, self)._check_one_user_type()
+        for user in self:
+            if (user.user_has_groups('account.group_show_line_subtotals_tax_included') and
+                    user.user_has_groups('account.group_show_line_subtotals_tax_excluded')):
+                raise ValidationError(_('A user cannot have both Tax B2B and Tax B2C'))

addons/account/models/__init__.py
@@ -19,3 +19,4 @@ from . import reconciliation_widget
 from . import account_incoterms
 from . import digest
 from . import account_invoice_import_wizard
+from . import res_users

commit 5e53af11c780ede31ed267ad01b6f6aa65eac718

[FIX] account: better message on user group error
The validation error introduced in f5cf2835 can pop up in contexts where
it is not expected;
e.g. not in editing user rights but while upgrading a module.
Therefore we make it more explicit, giving the user a way to resolve the error.

closes odoo/odoo#32623

Signed-off-by: Nans Lefebvre (len) <len@odoo.com>

addons/account/i18n/account.pot
@@ -657,7 +657,11 @@ msgstr ""
 #. module: account
 #: code:addons/account/models/res_users.py:17
 #, python-format
-msgid "A user cannot have both Tax B2B and Tax B2C"
+msgid "A user cannot have both Tax B2B and Tax B2C.\n"
+"Problematic user(s): %s\n"
+"You should go in General Settings, and choose to display Product Prices\n"
+"either in 'Tax-Included' or in 'Tax-Excluded' mode\n"
+"(or switch twice the mode if you are already in the desired one)."
 msgstr ""
 
 #. module: account

addons/account/models/res_users.py
@@ -12,7 +12,15 @@ class Users(models.Model):
     @api.constrains('groups_id')
     def _check_one_user_type(self):
         super(Users, self)._check_one_user_type()
-        for user in self:
-            if (user.user_has_groups('account.group_show_line_subtotals_tax_included') and
-                    user.user_has_groups('account.group_show_line_subtotals_tax_excluded')):
-                raise ValidationError(_('A user cannot have both Tax B2B and Tax B2C'))
+
+        users_with_both_groups = self.filtered(lambda user:
+            user.user_has_groups('account.group_show_line_subtotals_tax_included') and
+            user.user_has_groups('account.group_show_line_subtotals_tax_excluded')
+        )
+        if users_with_both_groups:
+            names = ", ".join(users_with_both_groups.mapped('name'))
+            raise ValidationError(_("A user cannot have both Tax B2B and Tax B2C.\n"
+                                    "Problematic user(s): %s\n"
+                                    "You should go in General Settings, and choose to display Product Prices\n"
+                                    "either in 'Tax-Included' or in 'Tax-Excluded' mode\n"
+                                    "(or switch twice the mode if you are already in the desired one).") % names)