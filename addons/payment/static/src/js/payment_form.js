YoushaExT/odoo

commit fb4cf5deccf5451fd36da60619c5125eaca86cb9

[IMP] payment*: better handling of error message
Sometimes there's a title + content, sometimes not; reading the message
itself usually yields a None somewhere which is fugly.

addons/payment/static/src/js/payment_form.js
@@ -139,6 +139,12 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
         $(button).children('.fa').addClass('fa-lock');
         $(button).find('span.o_loader').remove();
     },
+    _parseError: function(e) { 
+        if (e.message.data.arguments[1]) {
+            return e.message.data.arguments[0] + e.message.data.arguments[1];
+        }
+        return e.message.data.arguments[0];
+    },
 
     //--------------------------------------------------------------------------
     // Handlers
@@ -246,7 +252,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                     self.displayError(
                         _t('Server Error'),
                         _t("We are not able to add your payment method at the moment.") +
-                            error.message.data.message
+                            this._parseError(error)
                     );
                 });
             }
@@ -296,7 +302,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                         self.displayError(
                             _t('Server Error'),
                             _t("We are not able to redirect you to the payment form. ") +
-                                error.message.data.message
+                                this._parseError(error)
                         );
                     });
                 }
@@ -428,7 +434,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                 self.displayError(
                     _t('Server error'),
                     _t("We are not able to add your payment method at the moment.</p>") +
-                        error.message.data.message
+                        this._parseError(error)
                 );
             });
         }

addons/payment_authorize/static/src/js/payment_form.js
@@ -78,7 +78,7 @@ PaymentForm.include({
                 self.displayError(
                     _t('Server Error'),
                     _t("We are not able to add your payment method at the moment.") +
-                        error.message.data.message
+                        this._parseError(error)
                 );
             });
         };

addons/payment_stripe/static/src/js/payment_form.js
@@ -84,7 +84,7 @@ PaymentForm.include({
             self.displayError(
                 _t('Unable to save card'),
                 _t("We are not able to add your payment method at the moment. ") +
-                    error.message.data.message
+                    this._parseError(error)
             );
         });
     },

commit 0074c91c6721a02188038e2c4e7a5ea1185b2521

[FIX] payment: fix error management in payment form
PURPOSE

Test frontend and UI tools of eLearning.

SPECIFICATIONS

Recent commit fb4cf5deccf5451fd36da60619c5125eaca86cb9 introduced a tool method to parse errors and ease
their management. A variable typo was done by mistake, fixed in this commit.

LINKS

Task ID 1937768

closes odoo/odoo#36085

Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

addons/payment/static/src/js/payment_form.js
@@ -252,7 +252,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                     self.displayError(
                         _t('Server Error'),
                         _t("We are not able to add your payment method at the moment.") +
-                            this._parseError(error)
+                            self._parseError(error)
                     );
                 });
             }
@@ -302,7 +302,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                         self.displayError(
                             _t('Server Error'),
                             _t("We are not able to redirect you to the payment form. ") +
-                                this._parseError(error)
+                                self._parseError(error)
                         );
                     });
                 }
@@ -434,7 +434,7 @@ publicWidget.registry.PaymentForm = publicWidget.Widget.extend({
                 self.displayError(
                     _t('Server error'),
                     _t("We are not able to add your payment method at the moment.</p>") +
-                        this._parseError(error)
+                        self._parseError(error)
                 );
             });
         }

addons/payment_authorize/static/src/js/payment_form.js
@@ -78,7 +78,7 @@ PaymentForm.include({
                 self.displayError(
                     _t('Server Error'),
                     _t("We are not able to add your payment method at the moment.") +
-                        this._parseError(error)
+                        self._parseError(error)
                 );
             });
         };

addons/payment_stripe/static/src/js/payment_form.js
@@ -84,7 +84,7 @@ PaymentForm.include({
             self.displayError(
                 _t('Unable to save card'),
                 _t("We are not able to add your payment method at the moment. ") +
-                    this._parseError(error)
+                    self._parseError(error)
             );
         });
     },