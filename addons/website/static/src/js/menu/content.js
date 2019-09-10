YoushaExT/odoo

commit a8a2dcbfc91a3dfa979a8c60f5d8e1a0ddafcc9e

[IMP] website: add the ability to create mega menus
Allow users to create complex website menus, also known as "mega menus".
Mega menus are enabled individually via the edit menu dialog, then their
content can be customized by dragging snippets in them like any other
snippets area in edit mode.

task-1925319

closes odoo/odoo#36097

Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>


Co-authored-by: Andrea Ulliana <aul@odoo.com>

addons/website/static/src/js/menu/content.js
@@ -376,6 +376,8 @@ var MenuEntryDialog = weWidgets.LinkDialog.extend({
             text: data.name || '',
             isNewWindow: data.new_window,
         }, data || {}));
+
+        this.menuType = data.menuType;
     },
     /**
      * @override
@@ -389,6 +391,13 @@ var MenuEntryDialog = weWidgets.LinkDialog.extend({
 
         // Adapt URL label
         this.$('label[for="o_link_dialog_label_input"]').text(_t("Menu Label"));
+
+        // Auto add '#' URL and hide the input if for mega menu
+        if (this.menuType === 'mega') {
+            var $url = this.$('input[name="url"]');
+            $url.val('#').trigger('change');
+            $url.closest('.form-group').addClass('d-none');
+        }
 
         return this._super.apply(this, arguments);
     },
@@ -503,6 +512,10 @@ var EditMenuDialog = weWidgets.Dialog.extend({
             tolerance: 'pointer',
             attribute: 'data-menu-id',
             expression: '()(.+)', // nestedSortable takes the second match of an expression (*sigh*)
+            isAllowed: (placeholder, placeholderParent, currentItem) => {
+                return !placeholderParent
+                    || !currentItem[0].dataset.megaMenu && !placeholderParent[0].dataset.megaMenu;
+            },
         });
         return r;
     },
@@ -581,9 +594,13 @@ var EditMenuDialog = weWidgets.Dialog.extend({
      * dialog to edit this new menu.
      *
      * @private
+     * @param {Event} ev
      */
-    _onAddMenuButtonClick: function () {
-        var dialog = new MenuEntryDialog(this, {}, {});
+    _onAddMenuButtonClick: function (ev) {
+        var menuType = ev.currentTarget.dataset.type;
+        var dialog = new MenuEntryDialog(this, {}, {
+            menuType: menuType,
+        });
         dialog.on('save', this, link => {
             var newMenu = {
                 'fields': {
@@ -591,6 +608,7 @@ var EditMenuDialog = weWidgets.Dialog.extend({
                     'name': link.text,
                     'url': link.url,
                     'new_window': link.isNewWindow,
+                    'is_mega_menu': menuType === 'mega',
                     'sequence': 0,
                     'parent_id': false,
                 },
@@ -628,7 +646,9 @@ var EditMenuDialog = weWidgets.Dialog.extend({
         var menuID = $menu.data('menu-id');
         var menu = this.flat[menuID];
         if (menu) {
-            var dialog = new MenuEntryDialog(this, {}, menu.fields);
+            var dialog = new MenuEntryDialog(this, {}, _.extend({
+                menuType: menu.fields['is_mega_menu'] ? 'mega' : undefined,
+            }, menu.fields));
             dialog.on('save', this, link => {
                 _.extend(menu.fields, {
                     'name': link.text,

addons/website/data/website_data.xml
@@ -504,6 +504,12 @@
             <field name="type">url</field>
             <field name="url">/website/static/src/img/snippets_demo/s_team_member_4.png</field>
         </record>
+        <record id="website.s_mega_menu_menu_image_menu_default_image" model="ir.attachment">
+            <field name="public" eval="True"/>
+            <field name="name">s_mega_menu_menu_image_menu_default_image.png</field>
+            <field name="type">url</field>
+            <field name="url">/website/static/src/img/snippets_demo/s_references_5.png</field>
+        </record>
     </data>
     <data>
         <record id="group_multi_website" model="res.groups">

addons/website/models/website_menu.py
@@ -2,6 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.tools.translate import html_translate
 
 
 class Menu(models.Model):
@@ -16,6 +17,20 @@ class Menu(models.Model):
         menu = self.search([], limit=1, order="sequence DESC")
         return menu.sequence or 0
 
+    def _compute_field_is_mega_menu(self):
+        for menu in self:
+            menu.is_mega_menu = bool(menu.mega_menu_content)
+
+    def _set_field_is_mega_menu(self):
+        for menu in self:
+            if menu.is_mega_menu:
+                if not menu.mega_menu_content:
+                    default_content = self.env['ir.ui.view'].render_template('website.s_mega_menu_multi_menus')
+                    menu.mega_menu_content = default_content.decode()
+            else:
+                menu.mega_menu_content = False
+                menu.mega_menu_classes = False
+
     name = fields.Char('Menu', required=True, translate=True)
     url = fields.Char('Url', default='')
     page_id = fields.Many2one('website.page', 'Related Page', ondelete='cascade')
@@ -28,6 +43,9 @@ class Menu(models.Model):
     is_visible = fields.Boolean(compute='_compute_visible', string='Is Visible')
     group_ids = fields.Many2many('res.groups', string='Visible Groups',
                                  help="User need to be at least in one of these groups to see the menu")
+    is_mega_menu = fields.Boolean(compute=_compute_field_is_mega_menu, inverse=_set_field_is_mega_menu)
+    mega_menu_content = fields.Html(translate=html_translate, sanitize=False)
+    mega_menu_classes = fields.Char()
 
     def name_get(self):
         if not self._context.get('display_website') and not self.env.user.has_group('website.group_multi_website'):
@@ -115,6 +133,7 @@ class Menu(models.Model):
                     'name': node.name,
                     'url': node.page_id.url if node.page_id else node.url,
                     'new_window': node.new_window,
+                    'is_mega_menu': node.is_mega_menu,
                     'sequence': node.sequence,
                     'parent_id': node.parent_id.id,
                 },

addons/website/static/src/js/editor/wysiwyg_multizone.js
@@ -1,7 +1,38 @@
 odoo.define('web_editor.wysiwyg.multizone', function (require) {
 'use strict';
-var Wysiwyg = require('web_editor.wysiwyg');
 
+var Wysiwyg = require('web_editor.wysiwyg');
+var snippetsEditor = require('web_editor.snippet.editor');
+
+/**
+ * Show/hide the dropdowns associated to the given toggles and allows to wait
+ * for when it is fully shown/hidden.
+ *
+ * Note: this also takes care of the fact the 'toggle' method of bootstrap does
+ * not properly work in all cases.
+ *
+ * @param {jQuery} $toggles
+ * @param {boolean} [show]
+ * @returns {Promise<jQuery>}
+ */
+function toggleDropdown($toggles, show) {
+    return Promise.all(_.map($toggles, toggle => {
+        var $toggle = $(toggle);
+        var $dropdown = $toggle.parent();
+        var shown = $dropdown.hasClass('show');
+        if (shown === show) {
+            return;
+        }
+        var toShow = !shown;
+        return new Promise(resolve => {
+            $dropdown.one(
+                toShow ? 'shown.bs.dropdown' : 'hidden.bs.dropdown',
+                () => resolve()
+            );
+            $toggle.dropdown(toShow ? 'show' : 'hide');
+        });
+    })).then(() => $toggles);
+}
 
 /**
  * HtmlEditor
@@ -13,7 +44,6 @@ var Wysiwyg = require('web_editor.wysiwyg');
  *
  */
 var WysiwygMultizone = Wysiwyg.extend({
-
     /**
      * @override
      */
@@ -24,7 +54,35 @@ var WysiwygMultizone = Wysiwyg.extend({
             var outerHTML = this._getEscapedElement($el).prop('outerHTML');
             return self._saveElement(outerHTML, self.options.recordInfo, $el[0]);
         };
-        return this._super();
+
+        // Mega menu initialization: handle dropdown openings by hand
+        var $megaMenuToggles = this.$('.o_mega_menu_toggle');
+        $megaMenuToggles.removeAttr('data-toggle').dropdown('dispose');
+        $megaMenuToggles.on('click.wysiwyg_multizone', ev => {
+            var $toggle = $(ev.currentTarget);
+
+            // Each time we toggle a dropdown, we will destroy the dropdown
+            // behavior afterwards to keep manual control of it
+            var dispose = ($els => $els.dropdown('dispose'));
+
+            // First hide all other mega menus
+            toggleDropdown($megaMenuToggles.not($toggle), false).then(dispose);
+
+            // Then toggle the clicked one
+            toggleDropdown($toggle)
+                .then(dispose)
+                .then($el => {
+                    var isShown = $el.parent().hasClass('show');
+                    this.editor.snippetsMenu.toggleMegaMenuSnippets(isShown);
+                });
+        });
+
+        return this._super.apply(this, arguments).then(() => {
+            // Showing Mega Menu snippets if one dropdown is already opened
+            if (this.$('.o_mega_menu').hasClass('show')) {
+                this.editor.snippetsMenu.toggleMegaMenuSnippets(true);
+            }
+        });
     },
     /**
      * @override
@@ -32,20 +90,26 @@ var WysiwygMultizone = Wysiwyg.extend({
      */
     save: function () {
         if (this.isDirty()) {
-            return this.editor.save().then(function() {
-                return {isDirty: true};
-            });
+            return this._restoreMegaMenus()
+                .then(() => this.editor.save())
+                .then(() => ({isDirty: true}));
         } else {
             return {isDirty: false};
         }
     },
-
+    /**
+     * @override
+     */
+    destroy: function () {
+        this._restoreMegaMenus();
+        this._super.apply(this, arguments);
+    },
 
     //--------------------------------------------------------------------------
     // Private
     //--------------------------------------------------------------------------
 
-    _getEditableArea: function() {
+    _getEditableArea: function () {
         return $(':o_editable');
     },
     /**
@@ -59,8 +123,9 @@ var WysiwygMultizone = Wysiwyg.extend({
      *        page element)
      */
     _saveElement: function (outerHTML, recordInfo, editable) {
+        var promises = [];
         var $el = $(editable);
-        return this._rpc({
+        promises.push(this._rpc({
             model: 'ir.ui.view',
             method: 'save',
             args: [
@@ -69,7 +134,51 @@ var WysiwygMultizone = Wysiwyg.extend({
                 $el.data('oe-xpath') || null,
             ],
             context: recordInfo.context,
-        });
+        }));
+        if ($el.data('oe-field') === 'mega_menu_content') {
+            // On top of saving the mega menu content like any other field
+            // content, we must save the custom classes that were set on the
+            // menu itself.
+            // FIXME normally removing the 'show' class should not be necessary here
+            // TODO check that editor classes are removed here as well
+            var classes = _.without($el.attr('class').split(' '), 'dropdown-menu', 'o_mega_menu', 'show');
+            promises.push(this._rpc({
+                model: 'website.menu',
+                method: 'write',
+                args: [
+                    [parseInt($el.data('oe-id'))],
+                    {
+                        'mega_menu_classes': classes.join(' '),
+                    },
+                ],
+            }));
+        }
+        return Promise.all(promises);
+    },
+    /**
+     * Restores mega menu behaviors and closes them (important to do before
+     * saving otherwise they would be saved opened).
+     *
+     * @private
+     * @returns {Promise}
+     */
+    _restoreMegaMenus: function () {
+        var $megaMenuToggles = this.$('.o_mega_menu_toggle');
+        $megaMenuToggles.off('.wysiwyg_multizone')
+            .attr('data-toggle', 'dropdown')
+            .dropdown({});
+        return toggleDropdown($megaMenuToggles, false);
+    },
+});
+
+snippetsEditor.Class.include({
+    /**
+     * @private
+     * @param {boolean} show
+     */
+    toggleMegaMenuSnippets: function (show) {
+        setTimeout(() => this._activateSnippet(false));
+        this.$('#snippet_mega_menu').toggleClass('d-none', !show);
     },
 });
 

addons/website/static/src/scss/website.scss
@@ -1089,6 +1089,34 @@ table.table_desc tr td {
     }
 }
 
+// Mega menu
+.o_mega_menu {
+    width: 100%;
+    padding: 0;
+    margin-top: 0;
+
+    .container, .container-fluid {
+        // Need to reforce those because they are removed since its a container
+        // inside another container (the one in the navbar)
+        padding-left: $grid-gutter-width / 2;
+        padding-right: $grid-gutter-width / 2;
+    }
+}
+.o_mega_menu_container_size {
+    @include media-breakpoint-up(md) {
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    $-mm-max-widths: ();
+    @each $k, $v in $container-max-widths {
+        $-mm-max-widths: map-merge($-mm-max-widths, (
+            #{$k}: $v - $grid-gutter-width,
+        ));
+    }
+    @include make-container-max-widths($-mm-max-widths);
+}
+
 // Copyright
 .o_footer_copyright {
     padding: 8px;

addons/website/static/src/xml/website.contentMenu.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <templates id="template" xml:space="preserve">
 <t t-name="website.contentMenu.dialog.submenu">
-    <li t-att-data-menu-id="submenu.fields['id']">
+    <li t-att-data-menu-id="submenu.fields['id']" t-att-data-mega-menu="submenu.fields['is_mega_menu'] ? true : undefined">
         <div class="input-group">
             <div class="input-group-prepend">
                 <span class="input-group-text fa fa-bars" role="img" aria-label="Dropdown menu" title="Dropdown menu"/>
@@ -10,6 +10,7 @@
                 <span class="js_menu_label o_text_overflow flex-grow-1">
                     <t t-esc="submenu.fields['name']"/>
                 </span>
+                <span t-if="submenu.fields['is_mega_menu']" class="badge badge-primary">Mega Menu</span>
                 <i t-if="submenu.is_homepage" class="fa fa-home ml-3" role="img" aria-label="Home" title="Home"/>
             </span>
             <span class="input-group-append">
@@ -49,6 +50,9 @@
         </small>
         <a href="#" class="js_add_menu">
             <i class="fa fa-plus-circle"/> Add Menu Item
+        </a><br/>
+        <a href="#" class="js_add_menu" data-type="mega">
+            <i class="fa fa-plus-circle"/> Add Mega Menu Item
         </a>
     </div>
 </div>

commit 7932843f9394df7d1d9fdf13e33dde26c4d6865f

[FIX] website: fix nested menu creation
If the menu's parent is a newly created menu it will have a temp string
id which was failing to be parsed as an int and thus falling back on the
root menu as its parent.

This was broken by the new mega menu feature introduced with
https://github.com/odoo/odoo/commit/a8a2dcbfc91a3dfa979a8c60f5d8e1a0ddafcc9e

task-2069365

closes odoo/odoo#36606

Signed-off-by: Quentin Smetz (qsm) <qsm@odoo.com>

addons/website/static/src/js/menu/content.js
@@ -544,7 +544,7 @@ var EditMenuDialog = weWidgets.Dialog.extend({
                 levels[menu.depth] = (levels[menu.depth] || 0) + 1;
                 var menuFields = this.flat[menu.id].fields;
                 menuFields['sequence'] = levels[menu.depth];
-                menuFields['parent_id'] = parseInt(menu['parent_id']) || this.rootMenuID;
+                menuFields['parent_id'] = menu['parent_id'] || this.rootMenuID;
                 data.push(menuFields);
             }
         });