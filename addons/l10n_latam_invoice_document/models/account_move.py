YoushaExT/odoo

commit ef8ed61792d45f9e17146a08674be6ec9d9eed62

[IMP] account,* : Change type field to move_type in account.move
task-id: 2028z813

addons/l10n_latam_invoice_document/models/account_move.py
@@ -64,7 +64,7 @@ class AccountMove(models.Model):
 
     def post(self):
         for rec in self.filtered(lambda x: x.l10n_latam_use_documents and (not x.name or x.name == '/')):
-            if rec.type in ('in_receipt', 'out_receipt'):
+            if rec.move_type in ('in_receipt', 'out_receipt'):
                 raise UserError(_('We do not accept the usage of document types on receipts yet. '))
         return super().post()
 
@@ -94,11 +94,11 @@ class AccountMove(models.Model):
             raise ValidationError(_('The document number on the following invoices is not correct %s.' % (
                 without_number.ids)))
 
-    @api.constrains('type', 'l10n_latam_document_type_id')
+    @api.constrains('move_type', 'l10n_latam_document_type_id')
     def _check_invoice_type_document_type(self):
         for rec in self.filtered('l10n_latam_document_type_id.internal_type'):
             internal_type = rec.l10n_latam_document_type_id.internal_type
-            invoice_type = rec.type
+            invoice_type = rec.tmove_ype
             if internal_type in ['debit_note', 'invoice'] and invoice_type in ['out_refund', 'in_refund']:
                 raise ValidationError(_('You can not use a %s document type with a refund invoice') % internal_type)
             elif internal_type == 'credit_note' and invoice_type in ['out_invoice', 'in_invoice']:
@@ -106,13 +106,13 @@ class AccountMove(models.Model):
 
     def _get_l10n_latam_documents_domain(self):
         self.ensure_one()
-        if self.type in ['out_refund', 'in_refund']:
+        if self.move_type in ['out_refund', 'in_refund']:
             internal_types = ['credit_note']
         else:
             internal_types = ['invoice', 'debit_note']
         return [('internal_type', 'in', internal_types), ('country_id', '=', self.company_id.country_id.id)]
 
-    @api.depends('journal_id', 'partner_id', 'company_id', 'type')
+    @api.depends('journal_id', 'partner_id', 'company_id', 'move_type')
     def _compute_l10n_latam_available_document_types(self):
         self.l10n_latam_available_document_type_ids = False
         for rec in self.filtered(lambda x: x.journal_id and x.l10n_latam_use_documents and x.partner_id):
@@ -165,7 +165,7 @@ class AccountMove(models.Model):
         bills because the uniqueness must be per partner """
         for rec in self.filtered(lambda x: x.is_purchase_document() and x.l10n_latam_use_documents):
             domain = [
-                ('type', '=', rec.type),
+                ('move_type', '=', rec.move_type),
                 # by validating name we validate l10n_latam_document_type_id
                 ('name', '=', rec.name),
                 ('company_id', '=', rec.company_id.id),

addons/account_facturx/models/account_move.py
@@ -58,12 +58,12 @@ class AccountMove(models.Model):
         if self._context.get('default_journal_id'):
             journal = self.env['account.journal'].browse(self.env.context['default_journal_id'])
             default_type = 'out_invoice' if journal.type == 'sale' else 'in_invoice'
-        elif self._context.get('default_type'):
-            default_type = self._context['default_type']
-        elif self.type in self.env['account.move'].get_invoice_types(include_receipts=True):
+        elif self._context.get('default_move_type'):
+            default_type = self._context['default_move_type']
+        elif self.move_type in self.env['account.move'].get_invoice_types(include_receipts=True):
             # in case an attachment is saved on a draft invoice previously created, we might
             # have lost the default value in context but the type was already set
-            default_type = self.type
+            default_type = self.move_type
 
         if not default_type:
             raise UserError(_("No information about the journal or the type of invoice is passed"))
@@ -93,10 +93,10 @@ class AccountMove(models.Model):
             refund_sign = -1 if 'refund' in default_type else 1
 
         # Write the type as the journal entry is already created.
-        self.type = default_type
+        self.move_type = default_type
 
         # self could be a single record (editing) or be empty (new).
-        with Form(self.with_context(default_type=default_type)) as invoice_form:
+        with Form(self.with_context(default_move_type=default_type)) as invoice_form:
             # Partner (first step to avoid warning 'Warning! You must first select a partner.').
             partner_type = invoice_form.journal_id.type == 'purchase' and 'SellerTradeParty' or 'BuyerTradeParty'
             elements = tree.xpath('//ram:'+partner_type+'/ram:SpecifiedTaxRegistration/ram:ID', namespaces=tree.nsmap)
@@ -231,7 +231,7 @@ class AccountMove(models.Model):
         res = super(AccountMove, self).message_post(**kwargs)
 
         if not self.env.context.get('no_new_invoice') and len(self) == 1 and self.state == 'draft' and (
-            self.env.context.get('default_type', self.type) in self.env['account.move'].get_invoice_types(include_receipts=True)
+            self.env.context.get('default_move_type', self.move_type) in self.env['account.move'].get_invoice_types(include_receipts=True)
             or self.env['account.journal'].browse(self.env.context.get('default_journal_id')).type in ('sale', 'purchase')
         ):
             for attachment in self.env['ir.attachment'].browse(kwargs.get('attachment_ids', [])):

addons/l10n_ar/models/account_move.py
@@ -84,7 +84,7 @@ class AccountMove(models.Model):
         for inv in self.filtered(lambda x: x.company_id.l10n_ar_company_requires_vat):
             purchase_aliquots = 'not_zero'
             # we require a single vat on each invoice line except from some purchase documents
-            if inv.type in ['in_invoice', 'in_refund'] and inv.l10n_latam_document_type_id.purchase_aliquots == 'zero':
+            if inv.move_type in ['in_invoice', 'in_refund'] and inv.l10n_latam_document_type_id.purchase_aliquots == 'zero':
                 purchase_aliquots = 'zero'
             for line in inv.mapped('invoice_line_ids').filtered(lambda x: x.display_type not in ('line_section', 'line_note')):
                 vat_taxes = line.tax_ids.filtered(lambda x: x.tax_group_id.l10n_ar_vat_afip_code)

addons/l10n_cl/models/account_move.py
@@ -18,7 +18,7 @@ class AccountMove(models.Model):
         domain = super()._get_l10n_latam_documents_domain()
         if (self.journal_id.l10n_latam_use_documents and
                 self.journal_id.company_id.country_id == self.env.ref('base.cl')):
-            if self.type in ['in_invoice', 'in_refund']:
+            if self.move_type in ['in_invoice', 'in_refund']:
                 if self.partner_id.l10n_cl_sii_taxpayer_type == '2':
                     domain += [('code', '=', '71')]
                 return domain
@@ -28,7 +28,7 @@ class AccountMove(models.Model):
                 domain += [('code', 'in', ['35', '38', '39', '41', '56', '61'])]
         return domain
 
-    @api.constrains('type', 'l10n_latam_document_type_id')
+    @api.constrains('move_type', 'l10n_latam_document_type_id')
     def _check_invoice_type_document_type(self):
         super()._check_invoice_type_document_type()
         for rec in self.filtered(lambda r: r.company_id.country_id == self.env.ref(

addons/membership/models/account_move.py
@@ -13,7 +13,7 @@ class AccountMove(models.Model):
         # OVERRIDE to update the cancel date.
         res = super(AccountMove, self).button_draft()
         for move in self:
-            if move.type == 'out_invoice':
+            if move.move_type == 'out_invoice':
                 self.env['membership.membership_line'].search([
                     ('account_invoice_line', 'in', move.mapped('invoice_line_ids').ids)
                 ]).write({'date_cancel': False})
@@ -23,7 +23,7 @@ class AccountMove(models.Model):
         # OVERRIDE to update the cancel date.
         res = super(AccountMove, self).button_cancel()
         for move in self:
-            if move.type == 'out_invoice':
+            if move.move_type == 'out_invoice':
                 self.env['membership.membership_line'].search([
                     ('account_invoice_line', 'in', move.mapped('invoice_line_ids').ids)
                 ]).write({'date_cancel': fields.Date.today()})
@@ -46,7 +46,7 @@ class AccountMoveLine(models.Model):
         # OVERRIDE
         res = super(AccountMoveLine, self).write(vals)
 
-        to_process = self.filtered(lambda line: line.move_id.type == 'out_invoice' and line.product_id.membership)
+        to_process = self.filtered(lambda line: line.move_id.move_type == 'out_invoice' and line.product_id.membership)
 
         # Nothing to process, break.
         if not to_process:
@@ -82,7 +82,7 @@ class AccountMoveLine(models.Model):
     def create(self, vals_list):
         # OVERRIDE
         lines = super(AccountMoveLine, self).create(vals_list)
-        to_process = lines.filtered(lambda line: line.move_id.type == 'out_invoice' and line.product_id.membership)
+        to_process = lines.filtered(lambda line: line.move_id.move_type == 'out_invoice' and line.product_id.membership)
 
         # Nothing to process, break.
         if not to_process:

addons/point_of_sale/models/account_move.py
@@ -11,15 +11,15 @@ class AccountMove(models.Model):
 
     def _stock_account_get_last_step_stock_moves(self):
         stock_moves = super(AccountMove, self)._stock_account_get_last_step_stock_moves()
-        for invoice in self.filtered(lambda x: x.type == 'out_invoice'):
+        for invoice in self.filtered(lambda x: x.move_type == 'out_invoice'):
             stock_moves += invoice.sudo().mapped('pos_order_ids.picking_ids.move_lines').filtered(lambda x: x.state == 'done' and x.location_dest_id.usage == 'customer')
-        for invoice in self.filtered(lambda x: x.type == 'out_refund'):
+        for invoice in self.filtered(lambda x: x.move_type == 'out_refund'):
             stock_moves += invoice.sudo().mapped('pos_order_ids.picking_ids.move_lines').filtered(lambda x: x.state == 'done' and x.location_id.usage == 'customer')
         return stock_moves
 
     def _compute_amount(self):
         super(AccountMove, self)._compute_amount()
-        pos_invoices = self.filtered(lambda i: i.type in ['out_invoice', 'out_refund'] and i.pos_order_ids)
+        pos_invoices = self.filtered(lambda i: i.move_type in ['out_invoice', 'out_refund'] and i.pos_order_ids)
         for invoice in pos_invoices:
             invoice.payment_state = 'paid'
 

commit f766b4e61fb0f5c29ae1a13cf468011cf62362ce

[FIX] account, l10n_latam_invoice_document: fix move_type typo
Two typos were introduced with ef8ed61792d4.

closes odoo/odoo#45926

Signed-off-by: Laurent Smet <smetl@users.noreply.github.com>

addons/account/views/account_move_views.xml
@@ -1330,7 +1330,7 @@
             <field name="view_mode">tree,kanban,form</field>
             <field name="view_id" ref="view_invoice_tree"/>
             <field name="search_view_id" ref="view_account_invoice_filter"/>
-            <field name="domain">[('tmove_ype', '=', 'in_refund')]</field>
+            <field name="domain">[('move_type', '=', 'in_refund')]</field>
             <field name="context">{'default_move_type': 'in_refund'}</field>
             <field name="help" type="html">
               <p class="o_view_nocontent_smiling_face">

addons/l10n_latam_invoice_document/models/account_move.py
@@ -98,7 +98,7 @@ class AccountMove(models.Model):
     def _check_invoice_type_document_type(self):
         for rec in self.filtered('l10n_latam_document_type_id.internal_type'):
             internal_type = rec.l10n_latam_document_type_id.internal_type
-            invoice_type = rec.tmove_ype
+            invoice_type = rec.move_type
             if internal_type in ['debit_note', 'invoice'] and invoice_type in ['out_refund', 'in_refund']:
                 raise ValidationError(_('You can not use a %s document type with a refund invoice') % internal_type)
             elif internal_type == 'credit_note' and invoice_type in ['out_invoice', 'in_invoice']: