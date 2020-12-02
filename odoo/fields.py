YoushaExT/odoo

commit e99e11581c81e8bb3d0b59bcca09708515963463

[IMP] core: do not recompute stored field on new record
The use case is a cache miss of a stored field with compute on a new
record with origin.  The field should be computed only when explicitly
triggered, i.e., when a dependency has been modified.  Otherwise it
should be fetched from the origin record.

closes odoo/odoo#47353

Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

odoo/fields.py
@@ -979,8 +979,8 @@ class Field(MetaField('DummyField', (object,), {})):
             value = env.cache.get(record, self)
 
         except KeyError:
-            # real record
-            if record.id and self.store:
+            if self.store and record.id:
+                # real record: fetch from database
                 recs = record._in_cache_without(self)
                 try:
                     recs._fetch_field(self)
@@ -993,7 +993,13 @@ class Field(MetaField('DummyField', (object,), {})):
                     ]))
                 value = env.cache.get(record, self)
 
+            elif self.store and record._origin:
+                # new record with origin: fetch from origin
+                value = self.convert_to_cache(record._origin[self.name], record)
+                env.cache.set(record, self, value)
+
             elif self.compute:
+                # non-stored field or new record without origin: compute
                 if env.is_protected(self, record):
                     value = self.convert_to_cache(False, record, validate=False)
                     env.cache.set(record, self, value)
@@ -1005,12 +1011,8 @@ class Field(MetaField('DummyField', (object,), {})):
                         self.compute_value(record)
                     value = env.cache.get(record, self)
 
-            elif (not record.id) and record._origin:
-                value = self.convert_to_cache(record._origin[self.name], record)
-                env.cache.set(record, self, value)
-
-            elif (not record.id) and self.type == 'many2one' and self.delegate:
-                # the parent record is also a new record, with the same cached
+            elif self.type == 'many2one' and self.delegate and not record.id:
+                # parent record of a new record: new record, with the same
                 # values as record for the corresponding inherited fields
                 def is_inherited_field(name):
                     field = record._fields[name]
@@ -1025,15 +1027,16 @@ class Field(MetaField('DummyField', (object,), {})):
                 env.cache.set(record, self, value)
 
             else:
+                # non-stored field or stored field on new record: default value
                 value = self.convert_to_cache(False, record, validate=False)
                 env.cache.set(record, self, value)
                 defaults = record.default_get([self.name])
                 if self.name in defaults:
-                    # The null value above is necessary to convert x2many field values.
-                    # For instance, converting [(4, id)] accesses the field's current
-                    # value, then adds the given id. Without an initial value, the
-                    # conversion ends up here to determine the field's value, and this
-                    # generates an infinite recursion.
+                    # The null value above is necessary to convert x2many field
+                    # values. For instance, converting [(4, id)] accesses the
+                    # field's current value, then adds the given id. Without an
+                    # initial value, the conversion ends up here to determine
+                    # the field's value, and generates an infinite recursion.
                     value = self.convert_to_cache(defaults[self.name], record)
                     env.cache.set(record, self, value)
 

odoo/models.py
@@ -6142,8 +6142,12 @@ Record ids: %(records)s
         # make a snapshot based on the initial values of record
         snapshot0 = Snapshot(record, nametree)
 
-        # store changed values in cache, and update snapshot0
-        record._update_cache(changed_values, validate=False)
+        # store changed values in cache; also trigger recomputations based on
+        # subfields (e.g., line.a has been modified, line.b is computed stored
+        # and depends on line.a, but line.b is not in the form view)
+        record._update_cache(changed_values, validate=True)
+
+        # update snapshot0 with changed values
         for name in names:
             snapshot0.fetch(name)
 

odoo/addons/test_new_api/models/test_new_api.py
@@ -796,12 +796,18 @@ class ModelChildM2o(models.Model):
     parent_id = fields.Many2one('test_new_api.model_parent_m2o', ondelete='cascade')
     size1 = fields.Integer(compute='_compute_sizes', store=True)
     size2 = fields.Integer(compute='_compute_sizes', store=True)
+    cost = fields.Integer(compute='_compute_cost', store=True, readonly=False)
 
     @api.depends('parent_id.name')
     def _compute_sizes(self):
         for record in self:
             record.size1 = len(self.parent_id.name)
             record.size2 = len(self.parent_id.name)
+
+    @api.depends('name')
+    def _compute_cost(self):
+        for record in self:
+            record.cost = len(record.name)
 
     def write(self, vals):
         res = super(ModelChildM2o, self).write(vals)
@@ -816,6 +822,12 @@ class ModelParentM2o(models.Model):
 
     name = fields.Char('Name')
     child_ids = fields.One2many('test_new_api.model_child_m2o', 'parent_id', string="Children")
+    cost = fields.Integer(compute='_compute_cost', store=True)
+
+    @api.depends('child_ids.cost')
+    def _compute_cost(self):
+        for record in self:
+            record.cost = sum(child.cost for child in record.child_ids)
 
 
 class Country(models.Model):

odoo/addons/test_new_api/tests/test_new_fields.py
@@ -1090,6 +1090,52 @@ class TestFields(TransactionCaseWithUserDemo):
         with self.assertRaises(AccessError):
             cat1.name
 
+    def test_40_real_vs_new(self):
+        """ test field access on new records vs real records. """
+        Model = self.env['test_new_api.category']
+        real_record = Model.create({'name': 'Foo'})
+        self.env.cache.invalidate()
+        new_origin = Model.new({'name': 'Bar'}, origin=real_record)
+        new_record = Model.new({'name': 'Baz'})
+
+        # non-computed non-stored field: default value
+        real_record = real_record.with_context(default_dummy='WTF')
+        new_origin = new_origin.with_context(default_dummy='WTF')
+        new_record = new_record.with_context(default_dummy='WTF')
+        self.assertEqual(real_record.dummy, 'WTF')
+        self.assertEqual(new_origin.dummy, 'WTF')
+        self.assertEqual(new_record.dummy, 'WTF')
+
+        # non-computed stored field: origin or default if no origin
+        real_record = real_record.with_context(default_color=42)
+        new_origin = new_origin.with_context(default_color=42)
+        new_record = new_record.with_context(default_color=42)
+        self.assertEqual(real_record.color, 0)
+        self.assertEqual(new_origin.color, 0)
+        self.assertEqual(new_record.color, 42)
+
+        # computed non-stored field: always computed
+        self.assertEqual(real_record.display_name, 'Foo')
+        self.assertEqual(new_origin.display_name, 'Bar')
+        self.assertEqual(new_record.display_name, 'Baz')
+
+        # computed stored field: origin or computed if no origin
+        Model = self.env['test_new_api.recursive']
+        real_record = Model.create({'name': 'Foo'})
+        new_origin = Model.new({'name': 'Bar'}, origin=real_record)
+        new_record = Model.new({'name': 'Baz'})
+        self.assertEqual(real_record.display_name, 'Foo')
+        self.assertEqual(new_origin.display_name, 'Foo')
+        self.assertEqual(new_record.display_name, 'Baz')
+
+        # computed stored field with recomputation: always computed
+        real_record.name = 'Fool'
+        new_origin.name = 'Barr'
+        new_record.name = 'Bazz'
+        self.assertEqual(real_record.display_name, 'Fool')
+        self.assertEqual(new_origin.display_name, 'Barr')
+        self.assertEqual(new_record.display_name, 'Bazz')
+
     def test_40_new_defaults(self):
         """ Test new records with defaults. """
         user = self.env.user

odoo/addons/test_new_api/tests/test_onchange.py
@@ -678,3 +678,31 @@ class TestComputeOnchange(common.TransactionCase):
         form.foo = "foo6"
         self.assertEqual(form.bar, "foo6")
         self.assertEqual(form.baz, "baz5")
+
+    def test_onchange_one2many(self):
+        record = self.env['test_new_api.model_parent_m2o'].create({
+            'name': 'Family',
+            'child_ids': [
+                (0, 0, {'name': 'W', 'cost': 10}),
+                (0, 0, {'name': 'X', 'cost': 10}),
+                (0, 0, {'name': 'Y'}),
+                (0, 0, {'name': 'Z'}),
+            ],
+        })
+        record.flush()
+        self.assertEqual(record.child_ids.mapped('name'), list('WXYZ'))
+        self.assertEqual(record.cost, 22)
+
+        # modifying a line should not recompute the cost on other lines
+        with common.Form(record) as form:
+            with form.child_ids.edit(1) as line:
+                line.name = 'XXX'
+            self.assertEqual(form.cost, 15)
+
+            with form.child_ids.edit(1) as line:
+                line.cost = 20
+            self.assertEqual(form.cost, 32)
+
+            with form.child_ids.edit(2) as line:
+                line.cost = 30
+            self.assertEqual(form.cost, 61)

odoo/addons/test_new_api/views/test_new_api_views.xml
@@ -296,5 +296,28 @@
                 </form>
             </field>
         </record>
+
+        <!-- Multi form view -->
+        <record id="model_parent_form" model="ir.ui.view">
+            <field name="name">model parent form view</field>
+            <field name="model">test_new_api.model_parent_m2o</field>
+            <field name="arch" type="xml">
+                <form string="Model Parent">
+                    <sheet>
+                        <group>
+                            <field name="name"/>
+                            <field name="cost"/>
+                        </group>
+                        <label for="child_ids"/>
+                        <field name="child_ids">
+                            <tree string="Children" editable="bottom">
+                                <field name="name"/>
+                                <field name="cost"/>
+                            </tree>
+                        </field>
+                    </sheet>
+                </form>
+            </field>
+        </record>
     </data>
 </odoo>

commit d181b36bc87816e1a680d81b683aca30fb0e7771

[FIX] core: invalidation of computed editable field in one2many
This fixes the issue introduced by revision e99e11581c81e8bb3d0b59bcca09708515963463

The way computed fields are invalidated depends on the order of the dict
passed as a parameter to method onchange().  This causes some unexpected
behavior when dealing with computed editable fields: the user loses the
value in that field because of the onchange.

Explanation: during the onchange, the modified field is cached by

    record._update_cache(changed_values, validate=True)

If the field is a one2many, 'value' contains an update command for each
modified line.  Because of 'validate=True', the assignment triggers
field computations on the lines, which are already handled by another
call to onchange().  In case anything on the parent model modifies some
line, the whole one2many values are returned to the user, and
accidentally recomputed fields show up in the interface.

closes odoo/odoo#62748

Signed-off-by: Raphael Collet (rco) <rco@openerp.com>

odoo/fields.py
@@ -961,7 +961,8 @@ class Field(MetaField('DummyField', (object,), {})):
             #       not stored and not computed -> default
             #
             #   on a new record w/ origin:
-            #       stored -> fetch from origin (computation done above)
+            #       stored and not (computed and readonly) -> fetch from origin
+            #       stored and computed and readonly -> compute
             #       not stored and computed -> compute
             #       not stored and not computed -> default
             #
@@ -987,7 +988,7 @@ class Field(MetaField('DummyField', (object,), {})):
                     ]))
                 value = env.cache.get(record, self)
 
-            elif self.store and record._origin:
+            elif self.store and record._origin and not (self.compute and self.readonly):
                 # new record with origin: fetch from origin
                 value = self.convert_to_cache(record._origin[self.name], record)
                 env.cache.set(record, self, value)

odoo/models.py
@@ -6132,7 +6132,7 @@ Fields:
         # store changed values in cache; also trigger recomputations based on
         # subfields (e.g., line.a has been modified, line.b is computed stored
         # and depends on line.a, but line.b is not in the form view)
-        record._update_cache(changed_values, validate=True)
+        record._update_cache(changed_values, validate=False)
 
         # update snapshot0 with changed values
         for name in names:

odoo/addons/test_new_api/models/test_new_api.py
@@ -1147,3 +1147,35 @@ class ComputeMember(models.Model):
         container = self.env['test_new_api.compute.container']
         for member in self:
             member.container_id = container.search([('name', '=', member.name)], limit=1)
+
+
+class ComputeEditable(models.Model):
+    _name = _description = 'test_new_api.compute_editable'
+
+    line_ids = fields.One2many('test_new_api.compute_editable.line', 'parent_id')
+
+    @api.onchange('line_ids')
+    def _onchange_line_ids(self):
+        for line in self.line_ids:
+            # even if 'same' is not in the view, it should be the same as 'value'
+            line.count += line.same
+
+
+class ComputeEditableLine(models.Model):
+    _name = _description = 'test_new_api.compute_editable.line'
+
+    parent_id = fields.Many2one('test_new_api.compute_editable')
+    value = fields.Integer()
+    same = fields.Integer(compute='_compute_same', store=True)
+    edit = fields.Integer(compute='_compute_edit', store=True, readonly=False)
+    count = fields.Integer()
+
+    @api.depends('value')
+    def _compute_same(self):
+        for line in self:
+            line.same = line.value
+
+    @api.depends('value')
+    def _compute_edit(self):
+        for line in self:
+            line.edit = line.value

odoo/addons/test_new_api/tests/test_new_fields.py
@@ -1338,7 +1338,7 @@ class TestFields(TransactionCaseWithUserDemo):
         new_origin = Model.new({'name': 'Bar'}, origin=real_record)
         new_record = Model.new({'name': 'Baz'})
         self.assertEqual(real_record.display_name, 'Foo')
-        self.assertEqual(new_origin.display_name, 'Foo')
+        self.assertEqual(new_origin.display_name, 'Bar')
         self.assertEqual(new_record.display_name, 'Baz')
 
         # computed stored field with recomputation: always computed

odoo/addons/test_new_api/tests/test_onchange.py
@@ -4,7 +4,7 @@
 from unittest.mock import patch
 
 from odoo.addons.base.tests.common import SavepointCaseWithUserDemo
-from odoo.tests import common
+from odoo.tests import common, Form
 from odoo import Command
 
 def strip_prefix(prefix, names):
@@ -752,3 +752,39 @@ class TestComputeOnchange(common.TransactionCase):
             with form.child_ids.edit(2) as line:
                 line.cost = 30
             self.assertEqual(form.cost, 61)
+
+    def test_onchange_editable_compute_one2many(self):
+        # create a record with a computed editable field ('edit') on lines
+        record = self.env['test_new_api.compute_editable'].create({'line_ids': [(0, 0, {'value': 7})]})
+        record.flush()
+        line = record.line_ids
+        self.assertRecordValues(line, [{'value': 7, 'edit': 7, 'count': 0}])
+
+        # retrieve the onchange spec for calling 'onchange'
+        spec = Form(record)._view['onchange']
+
+        # The onchange on 'line_ids' should increment 'count' and keep the value
+        # of 'edit' (this field should not be recomputed), whatever the order of
+        # the fields in the dictionary.  This ensures that the value set by the
+        # user on a computed editable field on a line is not lost.
+        line_ids = [
+            Command.update(line.id, {'value': 8, 'edit': 9, 'count': 0}),
+            Command.create({'value': 8, 'edit': 9, 'count': 0}),
+        ]
+        result = record.onchange({'line_ids': line_ids}, 'line_ids', spec)
+        expected = {'value': {
+            'line_ids': [
+                Command.clear(),
+                Command.update(line.id, {'value': 8, 'edit': 9, 'count': 8}),
+                Command.create({'value': 8, 'edit': 9, 'count': 8}),
+            ],
+        }}
+        self.assertEqual(result, expected)
+
+        # change dict order in lines, and try again
+        line_ids = [
+            (op, id_, dict(reversed(list(vals.items()))))
+            for op, id_, vals in line_ids
+        ]
+        result = record.onchange({'line_ids': line_ids}, 'line_ids', spec)
+        self.assertEqual(result, expected)

odoo/addons/test_new_api/views/test_new_api_views.xml
@@ -341,5 +341,31 @@
                 </form>
             </field>
         </record>
+
+        <!-- compute_editable view -->
+        <record id="compute_editable_form" model="ir.ui.view">
+            <field name="name">test_new_api.compute_editable.form</field>
+            <field name="model">test_new_api.compute_editable</field>
+            <field name="arch" type="xml">
+                <form>
+                    <sheet>
+                        <field name="line_ids" mode="tree">
+                            <tree editable="bottom">
+                                <field name="value"/>
+                                <field name="edit"/>
+                                <field name="count"/>
+                            </tree>
+                            <form>
+                                <!-- this view makes sure that field "same" is not considered -->
+                                <field name="value"/>
+                                <field name="edit"/>
+                                <field name="count"/>
+                            </form>
+                        </field>
+                    </sheet>
+                </form>
+            </field>
+        </record>
+
     </data>
 </odoo>