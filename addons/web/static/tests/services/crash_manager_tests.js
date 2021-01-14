YoushaExT/odoo

commit c80574b49c7134f41b1eebf27f2584aea0d7978a

[IMP] web: use do_action instead of BBQ push_state on RedirectWarning
In the RedirectWarning exception handler, it is better
to use 'do_action()' instead of using a BBQ push_state() and then
a location.reload(), to preserve the context and avoid losing menus
and breadcrumbs for example.

As the RedirectWarning receives only the action ID and not the full
action, a new parameter has been added to be able to provide a context.
This context is added in the 'additional_context' parameter of the do_action()

TaskID: 2168071

addons/web/static/src/js/services/crash_manager.js
@@ -357,17 +357,17 @@ var RedirectWarningHandler = Widget.extend(ExceptionHandler, {
     display: function() {
         var self = this;
         var error = this.error;
+        var additional_context = _.extend({}, this.context, error.data.arguments[3]);
 
         new WarningDialog(this, {
             title: _.str.capitalize(error.type) || _t("Odoo Warning"),
             buttons: [
                 {text: error.data.arguments[2], classes : "btn-primary", click: function() {
-                    $.bbq.pushState({
-                        'action': error.data.arguments[1],
-                        'cids': $.bbq.getState().cids,
-                    }, 2);
-                    self.destroy();
-                    location.reload();
+                    self.do_action(
+                        error.data.arguments[1],
+                        {
+                            additional_context: additional_context,
+                        });
                 }},
                 {text: _t("Cancel"), click: function() { self.destroy(); }, close: true}
             ]

commit 55a57ce2a66af5486a73b2f3efe28330ae8ad158

[FIX] web: fix the RedirectWarningHandler dialog not closing
Impacted versions :
- 14.0 and higher

Steps to reproduce :
```
1. generate an odoo.exceptions.RedirectWarning
2. click on the primary button
```

Current behavior :
```
1. the action (argument of the RedirectWarning) is properly executed
2. the WarningDialog is not closed
```

Expected behavior :
```
1. the action should still be properly executed
2. the WarningDialog should be closed
```

Tests :
Added a test to ensure this behavior will not be overwritten in the future.
The test is for the CrashManager service, and evaluates the following sequence :
```
Entry: CrashManager receives a odoo.exceptions.RedirectWarning (code 200) via rpc_error
Asserts:
	- The RedirectWarning modal (WarningDialog) should be opened
	 	* Upon clicking on the primary button :
	- The modal should be closed
	- The action passed as an argument of the odoo.exceptions.RedirectWarning should be executed
```

Origin :
the incorrect behavior was introduced with :
https://github.com/odoo/odoo/commit/c80574b49c7134f41b1eebf27f2584aea0d7978a

Task ID: 2430100

closes odoo/odoo#64553

X-original-commit: 892598cb0335926ece12789d81673f5b6370a1fd
Signed-off-by: Thibault Delavallee (tde) <tde@openerp.com>

addons/web/static/src/js/services/crash_manager.js
@@ -360,7 +360,8 @@ var RedirectWarningHandler = Widget.extend(ExceptionHandler, {
                         {
                             additional_context: additional_context,
                         });
-                }},
+                        self.destroy();
+                }, close: true},
                 {text: _t("Cancel"), click: function() { self.destroy(); }, close: true}
             ]
         }, {

addons/web/views/webclient_templates.xml
@@ -698,6 +698,7 @@
         <script type="text/javascript" src="/web/static/tests/qweb_tests.js"></script>
         <script type="text/javascript" src="/web/static/tests/mockserver_tests.js"></script>
 
+        <script type="text/javascript" src="/web/static/tests/services/crash_manager_tests.js"></script>
         <script type="text/javascript" src="/web/static/tests/services/data_manager_tests.js"></script>
         <script type="text/javascript" src="/web/static/tests/services/notification_service_tests.js"></script>
 

addons/web/static/tests/services/crash_manager_tests.js
@@ -1,0 +1,62 @@
+odoo.define('web.crash_manager_tests', function (require) {
+    "use strict";
+    const CrashManager = require('web.CrashManager').CrashManager;
+    const Bus = require('web.Bus');
+    const testUtils = require('web.test_utils');
+    const core = require('web.core');
+    const createActionManager = testUtils.createActionManager;
+    
+QUnit.module('Services', {}, function() {
+
+    QUnit.module('CrashManager');
+
+    QUnit.test("Execute an action and close the RedirectWarning when clicking on the primary button", async function (assert) {
+        assert.expect(4);
+
+        var dummy_action_name = "crash_manager_tests_dummy_action";
+        var dummy_action = function() {
+                assert.step('do_action');
+            };
+        core.action_registry.add(dummy_action_name, dummy_action);
+
+        // What we want to test is a do-action triggered by the crashManagerService
+        // the intercept feature of testUtilsMock is not fit for this, because it is too low in the hierarchy
+        const bus = new Bus();
+        bus.on('do-action', null, payload => {
+            const { action, options } = payload;
+            actionManager.doAction(action, options);
+        });
+
+        var actionManager = await createActionManager({
+            actions: [dummy_action],
+            services: {
+                crash_manager: CrashManager,
+            },
+            bus
+        });
+        actionManager.call('crash_manager', 'rpc_error', {
+            code: 200,
+            data: {
+                name: "odoo.exceptions.RedirectWarning",
+                arguments: [
+                    "crash_manager_tests_warning_modal_text",
+                    dummy_action_name,
+                    "crash_manager_tests_button_text",
+                    null,
+                ]
+            }
+        });
+        await testUtils.nextTick();
+
+        var modal_selector = 'div.modal:contains("crash_manager_tests_warning_modal_text")';
+        assert.containsOnce($, modal_selector, "Warning Modal should be opened");
+
+        await testUtils.dom.click($(modal_selector).find('button.btn-primary'));
+
+        assert.containsNone($, modal_selector, "Warning Modal should be closed");
+        assert.verifySteps(['do_action'], "Warning Modal Primary Button Action should be executed");
+        
+        actionManager.destroy();
+    });
+});
+});