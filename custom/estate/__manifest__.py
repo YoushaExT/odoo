c0772a06df84b434da6bff990c542d9054a12a6c
Yousha Arshad <yousharunescape@yahoo.com>
2022-01-04 16:03:58 +0500

    indentation in manifest file

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -1,5 +1,10 @@
 {
- 'name': 'estate',
- 'depends': ['base'],
- 'application': True
+    'name': 'estate',
+    'depends': ['base'],
+    'data': [
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+        'security/ir.model.access.csv'
+    ],
+    'application': True
 }
