e37102f9fcd207a9f0d58e63b509a2f98c8e78de
Yousha Arshad <yousharunescape@yahoo.com>
2022-01-05 09:44:11 +0500

    change field displayed names using string kwarg

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -5,18 +5,18 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "This is an estate property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string='Title', required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(copy=False, default=fields.Date.today()+timedelta(days=90))
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string='Living Area (sqm)')
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
     garden_orientation = fields.Selection(selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
     state = fields.Selection(selection=[('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('canceled', 'Canceled')], required=True, copy=False, default='new')
     active = fields.Boolean(default=True)
