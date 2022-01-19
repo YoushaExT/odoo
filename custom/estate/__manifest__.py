b2469ced72008cb848ad63bcce656a48ba9987d2
Yousha Arshad <yousharunescape@yahoo.com>
2022-01-19 19:42:38 +0500

    add css

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -1,10 +1,15 @@
 {
     'name': 'estate',
-    'depends': ['base'],
+    'depends': ['base', 'web'],
     'data': [
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'security/ir.model.access.csv'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'estate/static/src/css/state_styling.scss',
+        ]
+    },
     'application': True
 }
