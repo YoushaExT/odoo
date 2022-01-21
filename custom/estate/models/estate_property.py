9a2f1aa02211aef84830f732731b4878f30aaf9b
Yousha Arshad <yousharunescape@yahoo.com>
2022-01-21 16:39:36 +0500

    property tags many2many - fully added

--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -24,4 +24,5 @@ class EstateProperty(models.Model):
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     buyer_id = fields.Many2one('res.partner')
     seller_id = fields.Many2one('res.users', string="Salesman", default=lambda self: self.env.user)
+    # tag_ids = fields.Many2many('estate.property.tag', string="Property Tags")
     tag_ids = fields.Many2many('estate.property.tag', string="Property Tags")
